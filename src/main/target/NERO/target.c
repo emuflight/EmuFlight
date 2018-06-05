5ef24e7c668ae0f49600a40fdc6551a5ce8d0d3d
jflyper <shinoda@jaist.ac.jp>
2018-06-05 17:56:34 +0900

    6ae6fe8a6808b8b6e270021c73d402b633e2893b

--- src/main/target/NERO/target.c
+++ src/main/target/NERO/target.c
@@ -39,12 +39,12 @@
 
 const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
     DEF_TIM(TIM3, CH2, PC7, TIM_USE_PPM,                 0, 0 ),
-    DEF_TIM(TIM5, CH1, PA0, TIM_USE_MOTOR,               0, 0 ), // D(1,2)
-    DEF_TIM(TIM5, CH2, PA1, TIM_USE_MOTOR,               0, 0 ), // D(1,4)
-    DEF_TIM(TIM5, CH3, PA2, TIM_USE_MOTOR,               0, 0 ), // D(1,0)
-    DEF_TIM(TIM5, CH4, PA3, TIM_USE_MOTOR,               0, 0 ), // *D(1,1) D(1,3)
-    DEF_TIM(TIM3, CH3, PB0, TIM_USE_MOTOR | TIM_USE_LED, 0, 0 ), // D(1,7)
-    DEF_TIM(TIM3, CH4, PB1, TIM_USE_MOTOR,               0, 0 ), // xD(1,2)
-    DEF_TIM(TIM8, CH3, PC8, TIM_USE_MOTOR,               0, 0 ), // *D(2,2) D(2,4)
-    DEF_TIM(TIM8, CH4, PC9, TIM_USE_MOTOR,               0, 0 ), // D(2,7)
+    DEF_TIM(TIM5, CH1, PA0, TIM_USE_MOTOR,               0, 0 ),
+    DEF_TIM(TIM5, CH2, PA1, TIM_USE_MOTOR,               0, 0 ),
+    DEF_TIM(TIM5, CH3, PA2, TIM_USE_MOTOR,               0, 0 ),
+    DEF_TIM(TIM5, CH4, PA3, TIM_USE_MOTOR,               0, 1 ),
+    DEF_TIM(TIM3, CH3, PB0, TIM_USE_MOTOR | TIM_USE_LED, 0, 0 ),
+    DEF_TIM(TIM3, CH4, PB1, TIM_USE_MOTOR,               0, 0 ),
+    DEF_TIM(TIM8, CH3, PC8, TIM_USE_MOTOR,               0, 1 ),
+    DEF_TIM(TIM8, CH4, PC9, TIM_USE_MOTOR,               0, 0 ),
 };
