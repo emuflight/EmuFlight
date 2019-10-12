05f722afa178c1b98670f81aeff29f731b94981f
Tom Hensel <github@jitter.eu>
2019-10-13 01:57:42 +0200

    first attempt to rise from the relics

--- .travis.sh
+++ .travis.sh
@@ -12,58 +12,8 @@ TRAVIS_BUILD_NUMBER=${TRAVIS_BUILD_NUMBER:=undefined}
 
 MAKE="make EXTRA_FLAGS=-Werror"
 
-CURL_BASEOPTS=(
-	"--retry" "10"
-	"--retry-max-time" "120" )
-
-CURL_PUB_BASEOPTS=(
-	"--form" "revision=${REVISION}"
-	"--form" "branch=${BRANCH}"
-	"--form" "travis_build_number=${TRAVIS_BUILD_NUMBER}"
-	"--form" "last_commit_date=${LAST_COMMIT_DATE}"
-	"--form" "github_repo=${TRAVIS_REPO_SLUG}"
-	"--form" "build_name=${BUILDNAME}" )
-
-# A hacky way of building the docs at the same time as the normal builds.
-if [ $PUBLISHDOCS ] ; then
-	if [ $PUBLISH_URL ] ; then
-
-		# Patch Gimli to fix underscores_inside_words
-		curl -L "${CURL_BASEOPTS[@]}" https://github.com/walle/gimli/archive/v0.5.9.tar.gz | tar zxf -
-
-		sed -i 's/).render(/, :no_intra_emphasis => true).render(/' gimli-0.5.9/ext/github_markup.rb
-
-		cd gimli-0.5.9/
-		gem build gimli.gemspec && gem install gimli
-		cd ../
-
-		./build_docs.sh
-
-		curl -k "${CURL_BASEOPTS[@]}" "${CURL_PUB_BASEOPTS[@]}" --form "manual=@docs/Manual.pdf" ${PUBLISH_URL} || true
-	fi
-
-elif [ $PUBLISHMETA ] ; then
-	if [ $PUBLISH_URL ] ; then
-		RECENT_COMMITS=$(git shortlog -n25)
-		curl -k "${CURL_BASEOPTS[@]}" "${CURL_PUB_BASEOPTS[@]}" --form "recent_commits=${RECENT_COMMITS}" ${PUBLISH_URL} || true
-	fi
-
 elif [ $TARGET ] ; then
     $MAKE $TARGET || exit $?
-
-	if [ $PUBLISH_URL ] ; then
-		if   [ -f ${TARGET_FILE}.bin ] ; then
-			TARGET_FILE=${TARGET_FILE}.bin
-		elif [ -f ${TARGET_FILE}.hex ] ; then
-			TARGET_FILE=${TARGET_FILE}.hex
-		else
-			echo "build artifact (hex or bin) for ${TARGET_FILE} not found, aborting";
-			exit 1
-		fi
-
-		curl -k "${CURL_BASEOPTS[@]}" "${CURL_PUB_BASEOPTS[@]}" --form "file=@${TARGET_FILE}" ${PUBLISH_URL} || true
-		exit 0;
-	fi
 
 elif [ $GOAL ] ; then
     if [ "test" == "$GOAL" ] ; then
