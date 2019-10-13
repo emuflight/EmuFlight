49ec2acde40838848f97397656687b57ba8ecc08
Tom Hensel <github@jitter.eu>
2019-10-13 22:21:33 +0200

    practice. practice more.

--- .travis.sh
+++ .travis.sh
@@ -3,11 +3,14 @@
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 #GIT_COMMIT_DATE=$(git log -1 --date=short --format="%cd")
 #GIT_REVISION=$(git rev-parse --short HEAD)
-TRAVIS_BUILD_NUMBER=${TRAVIS_BUILD_NUMBER:=0-}
+TRAVIS_BUILD_NUMBER=${TRAVIS_BUILD_NUMBER}
 #TRAVIS_REPO_SLUG=${TRAVIS_REPO_SLUG:=$USER/undefined}
 CODE_VERSION=$(make version)
 
-make EXTRA_FLAGS=-Werror $TARGET || exit $?
-jq --arg version "$CODE_VERSION-$TRAVIS_BUILD_NUMBER" --arg branch "$GIT_BRANCH" '.version.name = $version, .package.name = $branch' bintray-conf.json > bintray-conf.json
+make EXTRA_FLAGS=-Werror ${TARGET} || exit $?
+
+cat bintray-conf.json | jq \
+    --arg version "${CODE_VERSION}-${TRAVIS_BUILD_NUMBER}" --arg branch "${GIT_BRANCH}" \
+    '.version.name = $version, .package.name = $branch' > bintray-conf.json
 
 cat bintray-conf.json # DEBUG
