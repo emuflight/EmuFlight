bff21f405d0f34bacd9f9536d70b500b6b4f1031
Tom Hensel <github@jitter.eu>
2019-10-13 21:48:13 +0200

    get rid of the old whateverflight approach of doing things (not)

--- .travis.sh
+++ .travis.sh
@@ -1,35 +1,16 @@
-#!/bin/bash
+#!/bin/bash -x
 #
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-GIT_COMMIT_DATE=$(git log -1 --date=short --format="%cd")
-GIT_REVISION=$(git rev-parse --short HEAD)
+#GIT_COMMIT_DATE=$(git log -1 --date=short --format="%cd")
+#GIT_REVISION=$(git rev-parse --short HEAD)
 TRAVIS_BUILD_NUMBER=${TRAVIS_BUILD_NUMBER:=undefined}
-TRAVIS_REPO_SLUG=${TRAVIS_REPO_SLUG:=$USER/undefined}
+#TRAVIS_REPO_SLUG=${TRAVIS_REPO_SLUG:=$USER/undefined}
 CODE_VERSION=$(make version)
-#
+
 MAKE="make EXTRA_FLAGS=-Werror"
 
-if [ $TARGET ] ; then
-    $MAKE $TARGET || exit $?
-    jq --arg version "$CODE_VERSION.$TRAVIS_BUILD_NUMBER" --arg branch "$GIT_BRANCH" '.version.name = $version, .package.name = $branch' bintray-conf.json > bintray-conf.json
-    cat bintray-conf.json
+TARGET="$1"
+$MAKE $TARGET || exit $?
+jq --arg version "$CODE_VERSION-$TRAVIS_BUILD_NUMBER" --arg branch "$GIT_BRANCH" '.version.name = $version, .package.name = $branch' bintray-conf.json > bintray-conf.json
 
-elif [ $GOAL ] ; then
-    if [ "test" == "$GOAL" ] ; then
-        $MAKE check-target-independence || exit $?
-        $MAKE check-fastram-usage-correctness || exit $?
-    fi
-
-    $MAKE $GOAL || exit $?
-
-    if [ $PUBLISHCOV ] ; then
-        if [ "test" == "$GOAL" ] ; then
-            lcov --directory . -b src/test --capture --output-file coverage.info 2>&1 | grep -E ":version '402\*', prefer.*'406\*" --invert-match
-            lcov --remove coverage.info 'lib/test/*' 'src/test/*' '/usr/*' --output-file coverage.info
-            lcov --list coverage.info
-            coveralls-lcov coverage.info
-        fi
-    fi
-else
-    $MAKE all
-fi
+cat bintray-conf.json # DEBUG
