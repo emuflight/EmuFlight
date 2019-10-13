96d08ebe764362c0a018d1604f705a123da9c9b5
Tom Hensel <github@jitter.eu>
2019-10-13 23:23:47 +0200

    export. export. export!

--- .travis.sh
+++ .travis.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -x
 #
-GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+export GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 #GIT_COMMIT_DATE=$(git log -1 --date=short --format="%cd")
 #GIT_REVISION=$(git rev-parse --short HEAD)
 #TRAVIS_REPO_SLUG=${TRAVIS_REPO_SLUG:=$USER/undefined}
 
-VERSION="$(make version)-${TRAVIS_BUILD_NUMBER}"
+export VERSION="$(make version)-${TRAVIS_BUILD_NUMBER}"
 
 make EXTRA_FLAGS=-Werror ${TARGET} || exit $?
 
