da17a78fb9620bcfe3d5e8c0b49f9c92fda8e5a8
Tom Hensel <github@jitter.eu>
2019-10-13 23:15:39 +0200

    jq is not for me. bye jq. hi j2cli.

--- .travis.sh
+++ .travis.sh
@@ -3,14 +3,12 @@
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 #GIT_COMMIT_DATE=$(git log -1 --date=short --format="%cd")
 #GIT_REVISION=$(git rev-parse --short HEAD)
-TRAVIS_BUILD_NUMBER=${TRAVIS_BUILD_NUMBER}
 #TRAVIS_REPO_SLUG=${TRAVIS_REPO_SLUG:=$USER/undefined}
-CODE_VERSION=$(make version)
+
+VERSION="$(make version)-${TRAVIS_BUILD_NUMBER}"
 
 make EXTRA_FLAGS=-Werror ${TARGET} || exit $?
 
-cat bintray-conf.json | jq \
-    --arg version "${CODE_VERSION}-${TRAVIS_BUILD_NUMBER}" --arg branch "${GIT_BRANCH}" \
-    '.version.name = $version, .package.name = $branch' > bintray-conf.json
+j2 bintray-template.j2 bintray-conf.json
 
 cat bintray-conf.json # DEBUG
