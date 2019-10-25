777ed630e7b9db199d64539a0e78fc8be75e73ad
Quick-Flash <46289813+Quick-Flash@users.noreply.github.com>
2019-10-25 17:45:22 -0600

    Revert "older kalman with fix"
    This reverts commit 5ea7300470f84534e0b9e69f56fb082a13711ad9.

--- src/main/common/filter.c
+++ src/main/common/filter.c
@@ -278,6 +278,8 @@ FAST_CODE float fastKalmanUpdate(fastKalman_t *filter, float input)
     filter->x += k * (input - filter->x);
     filter->p = (1.0f - k) * filter->p;
 
+    filter->x = pt1FilterApply(&filter->lp_filter, filter->x);
+
     // update variance
     filter->window[filter->windowIndex] = input;
 
@@ -302,7 +304,7 @@ FAST_CODE float fastKalmanUpdate(fastKalman_t *filter, float input)
     {
     	if (/*setPoint != 0.0f &&*/ filter->oldSetPoint != setPoint)
     	{
-			const float cutoff_frequency = constrain(BASE_LPF_HZ * e, 10.0f, 500.0f);
+			const float cutoff_frequency = constrain(BASE_LPF_HZ * e, 50.0f, 260.0f);
 		    const float k = pt1FilterGain(cutoff_frequency, filter->updateRate);
 		    pt1FilterUpdateCutoff(&filter->lp_filter, k);
 		    filter->oldSetPoint = setPoint;
