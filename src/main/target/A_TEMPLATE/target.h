345d973e080a09dcf97b4a275960a379a5dcbf33
Kaiowarez <60309480+Kaiowarez@users.noreply.github.com>
2020-03-08 20:16:26 -0400

    Fixed AIKONF7 BB skip [ci]
    fixed aikon BB and baro , aikonf7 didnt have a baro

--- src/main/target/A_TEMPLATE/target.h
+++ src/main/target/A_TEMPLATE/target.h
@@ -1,60 +1,52 @@
-/*
- * This file is part of Cleanflight and Betaflight.
- *
- * Cleanflight and Betaflight are free software. You can redistribute
- * this software and/or modify this software under the terms of the
- * GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option)
- * any later version.
- *
- * Cleanflight and Betaflight are distributed in the hope that they
- * will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this software.
- *
- * If not, see <http://www.gnu.org/licenses/>.
- */
-
 #pragma once
+// manufacturer_id
 #define TARGET_BOARD_IDENTIFIER "AIKO"
+// board_name
 #define USBD_PRODUCT_STRING "AIKONF7"
 
+// set dshot_burst =
 #define ENABLE_DSHOT_DMAR       true
 
-#define LED0_PIN                  PC13
+// resource LED 1
+#define LED0_PIN                  PC10
+
+// resource BEEPER 1
 #define USE_BEEPER
 #define BEEPER_PIN                PC15
+// set beeper_inversion =
 #define BEEPER_INVERTED
+
+// resource PINIO 1
 #define USE_PINIO
 #define PINIO1_PIN                PC14
 #define USE_PINIOBOX
 
-//define camera control
+// resource CAMERA_CONTROL 1
 #define CAMERA_CONTROL_PIN        PB14
 
+// resource GYRO_EXTI 1
 #define USE_EXTI
 #define MPU_INT_EXTI              PC4
 #define USE_MPU_DATA_READY_SIGNAL
-//MPU-6000
+
 #define USE_ACC
 #define USE_ACC_SPI_MPU6000
 #define USE_GYRO
 #define USE_GYRO_SPI_MPU6000
-
 #define GYRO_MPU6000_ALIGN      CW0_DEG
 #define ACC_MPU6000_ALIGN       CW0_DEG
+// resource GYRO_CS 1
 #define MPU6000_CS_PIN          PA4
+// set gyro_1_spibus =
 #define MPU6000_SPI_INSTANCE    SPI1
 
-// ICM-20602
+// 2nd gyro
 #define USE_ACC_MPU6500
 #define USE_ACC_SPI_MPU6500
 #define USE_GYRO_MPU6500
 #define USE_GYRO_SPI_MPU6500
 
+// set gyro_1_sensor_align =
 #define ACC_MPU6500_ALIGN       CW0_DEG
 #define GYRO_MPU6500_ALIGN      CW0_DEG
 #define MPU6500_CS_PIN          SPI1_NSS_PIN
@@ -65,20 +57,28 @@
 #define USE_MAG_QMC5883
 #define MAG_I2C_INSTANCE         (I2CDEV_2)
 
+// osd
 #define USE_MAX7456
+// set max7456_spi_bus =
 #define MAX7456_SPI_INSTANCE    SPI2
+// resource OSD_CS
 #define MAX7456_SPI_CS_PIN      PB12
 #define MAX7456_SPI_CLK         (SPI_CLOCK_STANDARD) // 10MHz
 #define MAX7456_RESTORE_CLK     (SPI_CLOCK_FAST)
 
+// set blackbox_device = SPIFLASH
 #define ENABLE_BLACKBOX_LOGGING_ON_SPIFLASH_BY_DEFAULT
 #define USE_FLASHFS
 #define USE_FLASH_M25P16
+// resource FLASH_CS 1
 #define FLASH_CS_PIN            PB0
+// set flash_spi_bus =
 #define FLASH_SPI_INSTANCE      SPI3
 
 #define USE_VCP
 
+// resource SERIAL_TX 1
+// resource SERIAL_RX 1
 #define USE_UART1
 #define UART1_RX_PIN            PA10
 #define UART1_TX_PIN            PA9
@@ -98,25 +98,27 @@
 #define USE_UART5
 #define UART5_RX_PIN            PD2
 #define UART5_TX_PIN            PC12
-
-
-//#define USE_SOFTSERIAL1
+// number of uarts + 1 for usb
 #define SERIAL_PORT_COUNT       6 //VCP, USART1, USART2,USART3,USART4,USART5,USART6
+
 
 #define USE_ESCSERIAL
 #define ESCSERIAL_TIMER_TX_PIN  PB8 // (Hardware=0, PPM)
 
+// resource I2C_SDA/SCL 2
 #define USE_I2C
 #define I2C_DEVICE_2               (I2CDEV_2)
 #define USE_I2C_DEVICE_2
 #define I2C2_SCL                PB10
 #define I2C2_SDA                PB11
 
-/*
+
 #define USE_BARO
 #define BMP280_SPI_INSTANCE     SPI3
 #define BMP280_CS_PIN           PB2
-*/
+
+// resource SPI_SCK/MISO/MOSI
+// spi_nss_pin is declared with set " " = spi?
 #define USE_SPI
 #define USE_SPI_DEVICE_1
 #define USE_SPI_DEVICE_2
@@ -137,12 +139,19 @@
 #define SPI3_MISO_PIN            PB4
 #define SPI3_MOSI_PIN            PB5
 
+// set current_meter =
+// set battery meter =
+
 #define USE_ADC
 #define DEFAULT_VOLTAGE_METER_SOURCE VOLTAGE_METER_ADC
 #define DEFAULT_CURRENT_METER_SOURCE CURRENT_METER_ADC
+// resource ADC_CURR 1
 #define CURRENT_METER_ADC_PIN   PC1
+// resource ADC_BATT 1
 #define VBAT_ADC_PIN            PC0
+// resource ADC_RSSI 1
 #define RSSI_ADC_PIN            PC3
+// set
 #define CURRENT_METER_SCALE_DEFAULT 250                     // 3.3/120A  = 25mv/A
 
 #define BINDPLUG_PIN            PB2
@@ -154,5 +163,7 @@
 #define TARGET_IO_PORTC         0xffff
 #define TARGET_IO_PORTD         (BIT(2))
 
+// just number of timers declared
 #define USABLE_TIMER_CHANNEL_COUNT      9
+// TIM_N(#) # = TIM# declared in target.c
 #define USED_TIMERS ( TIM_N(2) | TIM_N(4) | TIM_N(8) | TIM_N(9) | TIM_N(12)   ) //update based on update CLRACINGF7 Target BF4.1+
