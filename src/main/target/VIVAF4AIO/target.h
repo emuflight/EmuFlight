63dcfd6045c54effa31670ad2125c564c1ba8cbc
Kaiowarez <60309480+Kaiowarez@users.noreply.github.com>
2020-04-10 12:14:34 -0400

    iFLIGHT, VIVAF4 FIXED

--- src/main/target/VIVAF4AIO/target.h
+++ src/main/target/VIVAF4AIO/target.h
@@ -127,7 +127,7 @@
 #define DEFAULT_CURRENT_METER_SOURCE CURRENT_METER_ADC
 #define CURRENT_METER_ADC_PIN   PC1
 #define VBAT_ADC_PIN            PC2
-#define RSSI_ADC_PIN            PA0
+#define RSSI_ADC_PIN            PC0
 #define CURRENT_METER_SCALE_DEFAULT 250                     // 3.3/120A  = 25mv/A
 
 #define DEFAULT_RX_FEATURE      FEATURE_RX_SERIAL
--- src/main/target/VIVAF4AIO/target.mk
+++ src/main/target/VIVAF4AIO/target.mk
@@ -3,7 +3,6 @@ FEATURES       = VCP ONBOARDFLASH
 TARGET_SRC = \
         		drivers/accgyro/accgyro_spi_mpu6500.c \
             drivers/accgyro/accgyro_mpu6500.c \
-            drivers/accgyro/accgyro_spi_mpu6000.c \
             drivers/barometer/barometer_bmp085.c \
             drivers/barometer/barometer_bmp280.c \
             drivers/barometer/barometer_ms5611.c \
