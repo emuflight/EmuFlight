3b973f8cfd422c564eb838cbdfdc277c1fb67fe0
Kaiowarez <60309480+Kaiowarez@users.noreply.github.com>
2020-04-30 00:02:08 -0400

    HMBF41S, Humming Bird
    target confirmed working

--- src/main/target/NBDHMBF41S/target.c
+++ src/main/target/NBDHMBF41S/target.c
@@ -1,39 +1,33 @@
 /*
- * This file is part of Cleanflight and Betaflight.
+ * This file is part of Cleanflight.
  *
- * Cleanflight and Betaflight are free software. You can redistribute
- * this software and/or modify this software under the terms of the
- * GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * Cleanflight is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Cleanflight and Betaflight are distributed in the hope that they
- * will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
+ * Cleanflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this software.
- *
- * If not, see <http://www.gnu.org/licenses/>.
+ * along with Cleanflight.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <stdint.h>
 
-#include "platform.h"
+#include <platform.h>
+#include "drivers/io.h"
 
 #include "drivers/dma.h"
-#include "drivers/io.h"
 #include "drivers/timer.h"
 #include "drivers/timer_def.h"
 
 const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
-
-    DEF_TIM(TIM4, CH4, PB9,  TIM_USE_MOTOR, 0, 0), // S1_OUT - DMA1_ST1
-    DEF_TIM(TIM4, CH2, PB7,  TIM_USE_MOTOR, 0, 0), // S3_OUT - DMA1_ST3
-    DEF_TIM(TIM4, CH1, PB6,  TIM_USE_MOTOR, 0, 0), // S2_OUT - DMA1_ST0
-    DEF_TIM(TIM4, CH3, PB8,  TIM_USE_MOTOR, 0, 0), // S4_OUT - DMA1_ST7
-
-    DEF_TIM(TIM5, CH1, PA0,  TIM_USE_LED,   0, 0), // 2812LED - DMA1_ST2
-
+    DEF_TIM(TIM4,  CH4, PB9, TIM_USE_MOTOR,       0, 0), // PWM1
+    DEF_TIM(TIM4,  CH2, PB7, TIM_USE_MOTOR,       0, 0), // PWM2
+    DEF_TIM(TIM4,  CH1, PB6, TIM_USE_MOTOR,       0, 0), // PWM3
+    DEF_TIM(TIM4,  CH3, PB8, TIM_USE_MOTOR,       0, 0), // PWM4
+    DEF_TIM(TIM5,  CH1, PA0, TIM_USE_LED,         0, 0),
 };
--- src/main/target/NBDHMBF41S/target.h
+++ src/main/target/NBDHMBF41S/target.h
@@ -23,79 +23,22 @@
 #define TARGET_BOARD_IDENTIFIER         "HMBF4" // Humming Bird
 #define USBD_PRODUCT_STRING             "Humming Bird"
 
-#define USE_LED_STRIP
-#define LED0_PIN                PC13
-#define LED1_PIN                PC14
+//#define USE_TARGET_CONFIG
 
-#define USE_BEEPER
-#define BEEPER_PIN              PB10
-#define BEEPER_INVERTED
-
-// *************** SPI1 Gyro & ACC **********************
-
+// *************** SPI *****************************
 #define USE_SPI
-
 #define USE_SPI_DEVICE_2
-#define SPI2_NSS_PIN            PB12
-#define SPI2_SCK_PIN            PB13
-#define SPI2_MISO_PIN           PB14
-#define SPI2_MOSI_PIN           PB15
-
 #define USE_SPI_DEVICE_3
-#define SPI3_NSS_PIN            PA15
-#define SPI3_SCK_PIN            PB3
-#define SPI3_MISO_PIN           PB4
-#define SPI3_MOSI_PIN           PB5
 
-#define MPU6000_CS_PIN          PA4
-#define MPU6000_SPI_INSTANCE    SPI3
+#define SPI2_NSS_PIN                    PB12
+#define SPI2_SCK_PIN                    PB13
+#define SPI2_MISO_PIN                   PB14
+#define SPI2_MOSI_PIN                   PB15
 
-#define USE_GYRO
-#define USE_GYRO_SPI_MPU6000
-#define USE_ACC
-#define USE_ACC_SPI_MPU6000
-#define ACC_MPU6000_ALIGN       CW90_DEG
-
-#define USE_EXTI
-#define MPU_INT_EXTI            PB0
-#define USE_MPU_DATA_READY_SIGNAL
-
-// *************** OSD *****************************
-
-#define USE_MAX7456
-#define MAX7456_SPI_INSTANCE    SPI3
-#define MAX7456_SPI_CS_PIN      SPI3_NSS_PIN
-
-// *************** CC2500 ***************************
-
-#define RX_SCK_PIN              SPI2_SCK_PIN
-#define RX_MISO_PIN             SPI2_MISO_PIN
-#define RX_MOSI_PIN             SPI2_MOSI_PIN
-#define RX_NSS_PIN              SPI2_NSS_PIN
-
-// #define RX_NSS_GPIO_CLK_PERIPHERAL   RCC_APB2Periph_GPIOA
-
-#define USE_RX_SPI
-#define RX_SPI_INSTANCE           SPI2
-
-#define RX_CC2500_SPI_DISABLE_CHIP_DETECTION
-#define RX_CC2500_SPI_GDO_0_PIN   PB2
-#define RX_CC2500_SPI_LED_PIN     PA13
-#define RX_CC2500_SPI_LED_PIN_INVERTED
-#define RX_FRSKY_SPI_LED_PIN_INVERTED
-#define RX_CC2500_SPI_TX_EN_PIN   PB10
-
-#define USE_RX_CC2500_SPI_DIVERSITY
-#define RX_CC2500_SPI_ANT_SEL_PIN PA7
-
-#define BINDPLUG_PIN              PC15
-
-#define USE_RX_FRSKY_SPI_D
-#define USE_RX_FRSKY_SPI_X
-#define USE_RX_SFHSS_SPI
-#define DEFAULT_RX_FEATURE      FEATURE_RX_SPI
-#define RX_SPI_DEFAULT_PROTOCOL RX_SPI_FRSKY_X
-#define USE_RX_FRSKY_SPI_TELEMETRY
+#define SPI3_NSS_PIN                    PA15
+#define SPI3_SCK_PIN                    PB3
+#define SPI3_MISO_PIN                   PB4
+#define SPI3_MOSI_PIN                   PB5
 
 // *************** UART *****************************
 #define USE_VCP
@@ -111,28 +54,79 @@
 #define SERIAL_PORT_COUNT               3
 #define USE_MSP_UART
 
+// *************** Gyro & ACC **********************
+#define USE_GYRO
+#define USE_ACC
+#define USE_GYRO_SPI_MPU6000
+#define USE_ACC_SPI_MPU6000
+
+#define MPU6000_CS_PIN                   PA4
+#define MPU6000_SPI_INSTANCE             SPI3
+#define GYRO_1_CS_PIN                    MPU6000_CS_PIN
+#define GYRO_1_SPI_INSTANCE              MPU6000_SPI_INSTANCE
+
+#define USE_EXTI
+#define MPU_INT_EXTI                    PB0
+#define GYRO_1_EXTI_PIN                 MPU_INT_EXTI
+#define USE_MPU_DATA_READY_SIGNAL
+
+#define GYRO_1_ALIGN                    CW90_DEG
+
+// *************** RX ******************************
+#define DJTS
+#define USE_RX_SPI
+#define RX_SPI_INSTANCE             SPI2
+#define RX_NSS_PIN                  SPI2_NSS_PIN
+#define RX_CC2500_SPI_GDO_0_PIN     PB2
+#define RX_CC2500_SPI_LED_PIN       PA13
+#define RX_CC2500_SPI_TX_EN_PIN     PB10
+#define RX_CC2500_SPI_ANT_SEL_PIN   PA7
+#define RX_SPI_BIND_PIN             PC15
+#define RX_CC2500_SPI_LNA_EN_PIN    NONE
+#define DEFAULT_RX_FEATURE          FEATURE_RX_SPI
+#define RX_SPI_DEFAULT_PROTOCOL     RX_SPI_FRSKY_D
+#define USE_RX_FRSKY_SPI_TELEMETRY
+#define USE_RX_CC2500_SPI_DIVERSITY
+#define USE_RX_CC2500_SPI_PA_LNA
+#define USE_RX_FRSKY_SPI_D
+#define USE_RX_FRSKY_SPI_X
+#define USE_RX_SFHSS_SPI
+
+// *************** OSD *****************************
+#define USE_MAX7456
+#define MAX7456_SPI_INSTANCE            SPI3
+#define MAX7456_SPI_CS_PIN              SPI3_NSS_PIN
+
 // *************** ADC *****************************
 #define USE_ADC
 #define ADC_INSTANCE                    ADC1
+#define VBAT_ADC_PIN                    PB1
+#define CURRENT_METER_ADC_PIN           PA5
+#define ADC1_DMA_STREAM           DMA2_Stream0
 #define ADC1_DMA_OPT                    0
-
-#define VBAT_ADC_PIN              PB1
-#define CURRENT_METER_ADC_PIN     PA5
-
-#define USE_ESCSERIAL
-
-#define DEFAULT_FEATURES                (FEATURE_OSD | FEATURE_TELEMETRY | FEATURE_LED_STRIP )
-#define DEFAULT_VOLTAGE_METER_SOURCE    VOLTAGE_METER_ADC
-#define DEFAULT_CURRENT_METER_SOURCE    CURRENT_METER_ADC
 #define CURRENT_METER_SCALE_DEFAULT     1020
 #define CURRENT_METER_OFFSET_DEFAULT    -50
 
-#define TARGET_IO_PORTA           0xffff
-#define TARGET_IO_PORTB           0xffff
-#define TARGET_IO_PORTC           0xffff
-#define TARGET_IO_PORTD           0xffff
+#define DEFAULT_VOLTAGE_METER_SOURCE    VOLTAGE_METER_ADC
+#define DEFAULT_CURRENT_METER_SOURCE    CURRENT_METER_ADC
 
-//#define  USE_DSHOT_DMA
+// *************** OTHERS **************************
+#define LED0_PIN                        PC13
+#define LED1_PIN                        PC14
 
-#define USABLE_TIMER_CHANNEL_COUNT 5
-#define USED_TIMERS             ( TIM_N(4) | TIM_N(5) )
+#define USE_BEEPER
+#define BEEPER_PIN                      PB10
+#define BEEPER_INVERTED
+
+#define USE_ESCSERIAL
+#define ENABLE_DSHOT_DMAR               true
+
+#define DEFAULT_FEATURES                (FEATURE_OSD | FEATURE_TELEMETRY | FEATURE_LED_STRIP)
+
+#define TARGET_IO_PORTA                 0xffff
+#define TARGET_IO_PORTB                 0xffff
+#define TARGET_IO_PORTC                 0xffff
+#define TARGET_IO_PORTD                 (BIT(2))
+
+#define USABLE_TIMER_CHANNEL_COUNT      5
+#define USED_TIMERS                     ( TIM_N(4)|TIM_N(5) )
