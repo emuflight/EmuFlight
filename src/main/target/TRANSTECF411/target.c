52f4b18aee0e1ed377a4f03960866067f54a81b3
Kaiowarez <60309480+Kaiowarez@users.noreply.github.com>
2020-07-05 07:22:46 -0400

    updating work
    Adding both Transtecf411 and f411hd

--- src/main/target/TRANSTECF411/target.c
+++ src/main/target/TRANSTECF411/target.c
@@ -28,10 +28,10 @@
 #include "drivers/timer_def.h"
 
 const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
-    DEF_TIM(TIM3, CH3, PB0,   TIM_USE_MOTOR,  0, 0), // PPM IN
-    DEF_TIM(TIM3, CH4, PB1,   TIM_USE_MOTOR,  0, 0), // S1_OUT
-    DEF_TIM(TIM2, CH3, PB10,  TIM_USE_MOTOR,  0, 0), // S2_OUT
-    DEF_TIM(TIM4, CH4, PB9,   TIM_USE_MOTOR,  0, 0), // S3_OUT
+    DEF_TIM(TIM3, CH3, PB0,   TIM_USE_MOTOR,  0, 0),    // S1_OUT
+    DEF_TIM(TIM3, CH4, PB1,   TIM_USE_MOTOR,  0, 0),    // S2_OUT
+    DEF_TIM(TIM2, CH3, PB10,  TIM_USE_MOTOR,  0, 0),    // S3_OUT
+    DEF_TIM(TIM4, CH4, PB9,   TIM_USE_MOTOR,  0, 0),    // S4_OUT
 
-    DEF_TIM(TIM1, CH1, PA8,  TIM_USE_LED,  0, 0), // S4_OUT
+    DEF_TIM(TIM1, CH1, PA8,   TIM_USE_LED,    0, 0),    // LED_Strip
 };
