dcaf000451ec3b8495ea7fdf07c1397748ce7a7e
Quick-Flash <46289813+Quick-Flash@users.noreply.github.com>
2020-08-05 09:42:28 -0600

    Quickflash iterm relax (#327)
    * iTermRelaxBF4.2
    
    * quickflash changes
    
    * Update pid.h
    
    * Update pid.c
    
    make it so itermRelaxFactor cant go below 0.
    
    * as Andrey suggested
    
    * copy-paste fix :)
    
    * Change names
    
    itermRelaxCutoff 	        ->  itermRelaxThreshold
    iterm_relax_cutoff	        ->  iterm_relax_threshold
    itermRelaxFrequency	->  itermRelaxCutoff
    iterm_relax_frequency	->  iterm_relax_cutoff
    
    * fix osd names
    
    * Update cms_menu_imu.c
    
    Co-authored-by: AndreySemjonov <50518855+AndreySemjonov@users.noreply.github.com>
    Co-authored-by: nerdCopter <56646290+nerdCopter@users.noreply.github.com>

--- src/main/flight/pid.c
+++ src/main/flight/pid.c
@@ -133,7 +133,7 @@ void resetPidProfile(pidProfile_t *pidProfile)
         .pidSumLimit = PIDSUM_LIMIT_MAX,
         .pidSumLimitYaw = PIDSUM_LIMIT_YAW,
         .dterm_filter_type = FILTER_PT1,
-        .itermWindupPointPercent = 50,
+        .itermWindupPointPercent = 70,
         .pidAtMinThrottle = PID_STABILISATION_ON,
         .levelAngleLimit = 45,
         .angleExpo = 10,
@@ -172,6 +172,10 @@ void resetPidProfile(pidProfile_t *pidProfile)
         .throttle_boost = 5,
         .throttle_boost_cutoff = 15,
         .iterm_rotation = true,
+        .iterm_relax_cutoff = 11,
+        .iterm_relax_cutoff_yaw = 25,
+        .iterm_relax_threshold = 35,
+        .iterm_relax_threshold_yaw = 35,
         .motor_output_limit = 100,
         .auto_profile_cell_count = AUTO_PROFILE_CELL_COUNT_STAY,
         .horizonTransition = 0,
@@ -210,6 +214,12 @@ static FAST_RAM filterApplyFnPtr dtermLowpassApplyFn = nullFilterApply;
 static FAST_RAM_ZERO_INIT dtermLowpass_t dtermLowpass[XYZ_AXIS_COUNT];
 static FAST_RAM filterApplyFnPtr dtermLowpass2ApplyFn = nullFilterApply;
 static FAST_RAM_ZERO_INIT dtermLowpass_t dtermLowpass2[XYZ_AXIS_COUNT];
+
+#if defined(USE_ITERM_RELAX)
+static FAST_RAM_ZERO_INIT pt1Filter_t windupLpf[XYZ_AXIS_COUNT];
+static FAST_RAM_ZERO_INIT uint8_t itermRelaxCutoff;
+static FAST_RAM_ZERO_INIT uint8_t itermRelaxCutoffYaw;
+#endif
 
 static FAST_RAM_ZERO_INIT float iDecay;
 
@@ -266,7 +276,18 @@ void pidInitFilters(const pidProfile_t *pidProfile)
 #if defined(USE_THROTTLE_BOOST)
     pt1FilterInit(&throttleLpf, pt1FilterGain(pidProfile->throttle_boost_cutoff, dT));
 #endif
+#if defined(USE_ITERM_RELAX)
+    for (int i = 0; i < XYZ_AXIS_COUNT; i++) {
+        if (i != FD_YAW) {
+            pt1FilterInit(&windupLpf[i], pt1FilterGain(itermRelaxCutoff, dT));
+        } else {
+            pt1FilterInit(&windupLpf[i], pt1FilterGain(itermRelaxCutoffYaw, dT));
+        }
+    }
+#endif
 }
+
+
 
 #ifdef USE_RC_SMOOTHING_FILTER
 void pidInitSetpointDerivativeLpf(uint16_t filterCutoff, uint8_t debugAxis, uint8_t filterType)
@@ -380,8 +401,11 @@ void pidInitConfig(const pidProfile_t *pidProfile)
     horizonCutoffDegrees = pidProfile->horizon_tilt_effect;
     maxVelocity[FD_ROLL] = maxVelocity[FD_PITCH] = pidProfile->rateAccelLimit * 100 * dT;
     maxVelocity[FD_YAW] = pidProfile->yawRateAccelLimit * 100 * dT;
-    const float ITermWindupPoint = (float)pidProfile->itermWindupPointPercent / 100.0f;
-    ITermWindupPointInv = 1.0f / (1.0f - ITermWindupPoint);
+    ITermWindupPointInv = 0.0f;
+    if (pidProfile->itermWindupPointPercent != 0) {
+      const float itermWindupPoint = pidProfile->itermWindupPointPercent / 100.0f;
+      ITermWindupPointInv = 1.0f / itermWindupPoint;
+    }
     crashTimeLimitUs = pidProfile->crash_time * 1000;
     crashTimeDelayUs = pidProfile->crash_delay * 1000;
     crashRecoveryAngleDeciDegrees = pidProfile->crash_recovery_angle * 10;
@@ -395,6 +419,10 @@ void pidInitConfig(const pidProfile_t *pidProfile)
     throttleBoost = pidProfile->throttle_boost * 0.1f;
 #endif
     itermRotation = pidProfile->iterm_rotation;
+#if defined(USE_ITERM_RELAX)
+    itermRelaxCutoff = pidProfile->iterm_relax_cutoff;
+    itermRelaxCutoffYaw = pidProfile->iterm_relax_cutoff_yaw;
+#endif
     iDecay = (float)pidProfile->i_decay;
 }
 
@@ -657,7 +685,11 @@ void pidController(const pidProfile_t *pidProfile, const rollAndPitchTrims_t *an
     vbatCompensationFactor = scaleRangef(currentControlRateProfile->vbat_comp_pid_level, 0.0f, 100.0f, 1.0f, vbatCompensationFactor);
 
     // gradually scale back integration when above windup point
-    const float dynCi = constrainf((1.0f - getMotorMixRange()) * ITermWindupPointInv, 0.0f, 1.0f) * dT;
+    float dynCi = dT;
+    if (ITermWindupPointInv != 0.0f)
+    {
+      dynCi *= constrainf((1.0f - getMotorMixRange()) * ITermWindupPointInv, 0.0f, 1.0f);
+    }
     float errorRate;
 
     // ----------PID controller----------
@@ -733,14 +765,37 @@ void pidController(const pidProfile_t *pidProfile, const rollAndPitchTrims_t *an
         // 2-DOF PID controller with optional filter on derivative term.
         // derivative term can be based on measurement or error using a sliding value from 0-100
 
+        float itermErrorRate = boostedErrorRate + errorRate;
+        float iterm          = temporaryIterm[axis];
+#if defined(USE_ITERM_RELAX)
+        float itermRelaxFactor;
+        if ((itermRelaxCutoff && axis != FD_YAW) || (itermRelaxCutoffYaw && axis == FD_YAW)) {
+            const float setpointLpf = pt1FilterApply(&windupLpf[axis], currentPidSetpoint);
+            const float setpointHpf = fabsf(currentPidSetpoint - setpointLpf);
+            if (axis != FD_YAW)
+            {
+                itermRelaxFactor = MAX(1 - setpointHpf / pidProfile->iterm_relax_threshold, 0.0f);
+            } else {
+                itermRelaxFactor = MAX(1 - setpointHpf / pidProfile->iterm_relax_threshold_yaw, 0.0f);
+            }
+            if (SIGN(iterm) == SIGN(itermErrorRate)) {
+                itermErrorRate *= itermRelaxFactor;
+            }
+            if (axis == FD_ROLL) {
+                DEBUG_SET(DEBUG_ITERM_RELAX, 0, lrintf(setpointHpf));
+                DEBUG_SET(DEBUG_ITERM_RELAX, 1, lrintf(itermRelaxFactor * 100.0f));
+                DEBUG_SET(DEBUG_ITERM_RELAX, 2, lrintf(itermErrorRate));
+            }
+        }
+#endif // USE_ITERM_RELAX
+
         // -----calculate P component
         pidData[axis].P = (pidCoefficient[axis].Kp * (boostedErrorRate + errorRate)) * vbatCompensationFactor;
 
         // -----calculate I component
         //float iterm = constrainf(pidData[axis].I + (pidCoefficient[axis].Ki * errorRate) * dynCi, -itermLimit, itermLimit);
-        float iterm    = temporaryIterm[axis];
         float iDecayMultiplier = iDecay;
-        float ITermNew = pidCoefficient[axis].Ki * (boostedErrorRate + errorRate) * dynCi;
+        float ITermNew = pidCoefficient[axis].Ki * itermErrorRate * dynCi;
         if (ITermNew != 0.0f)
         {
             if (SIGN(iterm) != SIGN(ITermNew))
