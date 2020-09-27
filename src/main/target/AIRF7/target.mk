54ccd8ec60aac537021f79e5fa0fcecb8d6eaf8a
Kaiowarez <60309480+Kaiowarez@users.noreply.github.com>
2020-09-27 12:01:28 -0400

    Fixed AIRF7, CLRACINGF7, FURYF4 (#361)
    fixing AIRF7 gyro and accel resource
    added clracingF7 pinio fix as well
    FURYF4OSD added timer as per BF targets list update

--- src/main/target/AIRF7/target.mk
+++ src/main/target/AIRF7/target.mk
@@ -3,8 +3,6 @@ FEATURES        += VCP ONBOARDFLASH
 
 TARGET_SRC = \
             drivers/accgyro/accgyro_spi_mpu6000.c \
-            drivers/accgyro/accgyro_mpu6500.c \
-            drivers/accgyro/accgyro_spi_mpu6500.c \
             drivers/barometer/barometer_bmp280.c \
             drivers/accgyro/accgyro_spi_icm20689.c \
             drivers/max7456.c
