6d7b5478f46c7edac3011a989cc1916272da4f28
Kaiowarez <60309480+Kaiowarez@users.noreply.github.com>
2020-10-01 21:04:17 -0400

    idk what to name this...

--- src/main/io/osd.h
+++ src/main/io/osd.h
@@ -179,11 +179,10 @@ typedef struct osdConfig_s {
     // Alarms
     uint16_t cap_alarm;
     uint16_t alt_alarm;
+    uint8_t lq_alarm;
     uint8_t rssi_alarm;
-uint16_t distance_alarm;
-
+    uint16_t distance_alarm;
     osd_unit_e units;
-
     uint16_t timers[OSD_TIMER_COUNT];
     uint16_t enabledWarnings;
 
