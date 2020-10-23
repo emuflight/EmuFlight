1b9254bf6c0885f0768cf7dfc4628f5b5b5fe02d
madchiller <DJ.madchiller@gmail.com>
2020-10-22 20:46:08 -0400

    Update Target.mk to use nbd7456

--- src/main/target/HMBF4_PRO/target.mk
+++ src/main/target/HMBF4_PRO/target.mk
@@ -4,7 +4,7 @@ TARGET_SRC  =                                       \
              drivers/accgyro/accgyro_mpu.c          \
              drivers/accgyro/accgyro_spi_bmi160.c \
              drivers/accgyro/accgyro_spi_mpu6000.c  \
-             drivers/max7456.c                      \
+             drivers/nbd7456.c                      \
              drivers/rx/rx_cc2500.c                 \
              rx/cc2500_common.c                     \
              rx/cc2500_sfhss.c                      \
