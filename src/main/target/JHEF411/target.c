61b6117305aa9b63f7162256a16b47746cc5ad2a
Kaiowarez <60309480+Kaiowarez@users.noreply.github.com>
2020-12-06 10:18:10 -0500

    Add Target, some minor fixes for other targets

--- src/main/target/JHEF411/target.c
+++ src/main/target/JHEF411/target.c
@@ -19,19 +19,26 @@
  */
 
 #include <stdint.h>
+
 #include "platform.h"
 #include "drivers/io.h"
+
 #include "drivers/dma.h"
 #include "drivers/timer.h"
 #include "drivers/timer_def.h"
 
 const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
+    DEF_TIM(TIM9, CH1, PA2,   TIM_USE_PPM,   0, 0), // PPM IN
 
-    DEF_TIM(TIM11, CH1,  PB9, TIM_USE_ANY,     0, 0), // CAMERA_CONTROL_PIN
+    DEF_TIM(TIM1, CH1, PA8,  TIM_USE_MOTOR,  0, 1), // S1_OUT 2,1
+    DEF_TIM(TIM1, CH2, PA9,  TIM_USE_MOTOR,  0, 1), // S2_OUT 2,2
+    DEF_TIM(TIM1, CH3, PA10, TIM_USE_MOTOR,  0, 0), // S3_OUT 2,6
+    DEF_TIM(TIM3, CH3, PB0,  TIM_USE_MOTOR,  0, 0), // S4_OUT 1,7
+    DEF_TIM(TIM3, CH1, PB4,  TIM_USE_MOTOR, 0, 0), //  S5_OUT 1,4
+    //DEF_TIM(TIM3, CH2, PB5,  TIM_USE_MOTOR, 0, 0), //  S6_OUT 1,5
 
-    DEF_TIM(TIM3,  CH4,  PB1, TIM_USE_MOTOR,   0, 0),  // M1_OUT - D1_ST2_CH5
-    DEF_TIM(TIM8,  CH2N, PB0, TIM_USE_MOTOR,   0, 0),  // M2_OUT - D2_ST3_CH7
-    DEF_TIM(TIM8,  CH4,  PC9, TIM_USE_MOTOR,   0, 0),  // M3_OUT - D2_ST7_CH7
-    DEF_TIM(TIM1,  CH1,  PA8, TIM_USE_MOTOR,   0, 1),  // M4_OUT - D2_ST1_CH6
-    DEF_TIM(TIM4,  CH3,  PB8, TIM_USE_LED,    0,  0)     // LED & MOTOR5 D1_ST7_CH2
+    DEF_TIM(TIM3, CH4, PB1,  TIM_USE_ANY,   0, 0), //  RSSI   1,2
+    DEF_TIM(TIM5, CH4, PA3,  TIM_USE_ANY,   0, 1), //  RX2    1,0
+    DEF_TIM(TIM2, CH3, PB10, TIM_USE_ANY,   0, 0), //  CAM CC 1,1
+    DEF_TIM(TIM2, CH1, PA15, TIM_USE_LED,   0, 0), //  LED    1,5
 };
