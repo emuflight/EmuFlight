33916959a92d8e23372536c2d463b4e6d06275a0
Quick-Flash <46289813+Quick-Flash@users.noreply.github.com>
2020-12-06 09:30:02 -0700

    Update pid.c

--- src/main/flight/pid.c
+++ src/main/flight/pid.c
@@ -780,12 +780,14 @@ void FAST_CODE pidController(const pidProfile_t *pidProfile)
                 DEBUG_SET(DEBUG_ABG, 0, lrintf(delta));
             } else if (axis == FD_PITCH) {
                 DEBUG_SET(DEBUG_ABG, 2, lrintf(delta));
+            }
             delta = pidRuntime.dtermABGApplyFn((filter_t *) &pidRuntime.dtermABG[axis], delta);
             if (axis == FD_ROLL) {
                 DEBUG_SET(DEBUG_ABG, 1, lrintf(delta));
             } else if (axis == FD_PITCH) {
                 DEBUG_SET(DEBUG_ABG, 3, lrintf(delta));
-
+            }
+            
             if (axis == FD_ROLL) {
                 DEBUG_SET(DEBUG_D_LPF, 1, lrintf(delta));
             } else if (axis == FD_PITCH) {
