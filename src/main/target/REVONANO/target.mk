1f5d9e98ae06fe569915103568b2840ba5e1618a
Quick-Flash <46289813+Quick-Flash@users.noreply.github.com>
2020-12-30 12:13:26 -0700

    this time pull in code and keep history

--- src/main/target/REVONANO/target.mk
+++
@@ -1,7 +0,0 @@
-F411_TARGETS   += $(TARGET)
-FEATURES       += VCP ONBOARDFLASH
-
-TARGET_SRC = \
-           drivers/accgyro/accgyro_mpu6500.c \
-           drivers/accgyro/accgyro_spi_mpu6500.c \
-           drivers/barometer/barometer_ms5611.c
--- src/test/unit/timer_definition_unittest.include/pg/bus_i2c.h
+++
@@ -1,5 +0,0 @@
-#define I2CDEV_2 (1)
-
-int i2cConfig(int);
-void i2cHardwareConfigure(int);
-void i2cInit(int);
