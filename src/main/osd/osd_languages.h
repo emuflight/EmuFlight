1d71ed1b117504c5bdf52fdeac5f44f9b80bf725
Franz-Robert <github@zeno.nu>
2021-02-17 21:14:20 +0100

    Include some Dutch in OSD languages

--- src/main/osd/osd_languages.h
+++ src/main/osd/osd_languages.h
@@ -116,6 +116,93 @@
 // text from cms_menu_vtx_tramp.c
 // text from cms.c
 
+#elif defined(DUTCH_OSD)
+// text from osd_elements.c
+#define AG "az" // shortened version of antigravity (anti-zwaartekracht)
+#define CRAFTNAME "VOERTUIG_NAAM"
+#define DBG "DBG %5d %5d %5d %5d" // keep the same except for the DBG which is short for debug
+#define DISARMED "ONTWAPEND"
+#define DISPLAYNAME "WEERGAVE_NAAM"
+#define RATE "RATE_%u" // only change the RATE portion
+#define FS "!FS!" // only change the FS part which is short for failsafe, keep 4 characters long which inlcude the ! !
+#define RESC "RESC" // RESC is short for rescue, keep 4 characters long
+#define HEAD "HEAD" // HEAD is short for headfree mode, keep 4 characters long
+#define ANGL "ANGL" // short for angle mode, keep 4 characters long
+#define HOR "HOR " // short for horizon mode, keep 4 characters long
+#define NFE "NFE " // stands for NFE mode, keep 4 characters long
+#define AIR "AIR " // short for airmode, keep 4 characters long
+#define ACRO "ACRO" // short for acro mode, keep 4 characters long
+#define PIT "PIT" // short for pitch, keep 3 characters long
+#define ROL "ROL" // short for roll, keep 3 characters long
+#define YA "YAW" // keep 3 characters long
+#define BEACON_ON " BEACON AAN" // keep the space and try to keep the same length
+#define ARM_IN "INSCHAKELEN IN %d.%d" // only change the ARM IN keep the rest the same.
+#define FAIL_SAFE "FAIL SAFE"
+#define CRASH_FLIP "CRASH FLIP" // also known as turtle mode, keep naming consistent with crash flip
+#define LAUNCH_ANGLE "LANCEER %d" // only change the LAUNCH portion
+#define LAUNCH "LANCEER"
+#define RSSI_LOW "RSSI LAAG"
+#define RSSI_DBM "RSSI DBM"
+#define LINK_QUALITY "LINK KWALITEIT"
+#define LAND_NOW " LAND NU"
+#define RESCUE_NA "REDDING N/B"
+#define RESCUE_OFF "REDDING UIT"
+#define HEADFREE "HEADFREE"
+#define ACC_RECOVERY "ACC RECOVERY"
+#define CORE "CORE %c: %3d%c" // only change the CORE portion
+#define LOW_BATTERY "BATTERIJ LAAG"
+#define RCSMOOTHING "RCSMOOTHING"
+#define OVER_CAP "OVER CAP" // short for over capacity, as in your battery capacity
+#define BATT_LESS_FULL "BATT < VOL" // short for battery < full, keep the <
+// text from osd.c
+#define ON_TIME "AAN TIJD " // keep the length the same
+#define TOTAL_ARM "TOTAL ARM" // keep the length the same
+#define LAST_ARM "LAST ARM " // keep the length the same
+#define ON_ARM "ON/ARM   " // keep the length the same
+#define FAULT "FOUT"
+#define NO_RTC "GEEN RTC" // stands for no real time clock
+#define MAX_ALTITUDE "MAX HOOGTE"
+#define MAX_SPEED "MAX SNELHEID"
+#define MAX_DISTANCE "MAX AFSTAND"
+#define FLIGHT_DISTANCE "AFSTAND VAN VLUCHT"
+#define MIN_BATTERY "MIN BATTERIJ"
+#define END_BATTERY "EIND BATTERIJ"
+#define BATTERY "BATTERIJ"
+#define MIN_RSSI "MIN RSSI"
+#define MAX_CURRENT "MAX STROOM"
+#define USED_MAH "GEBRUIKTE MAH"
+#define BLACKBOX "BLACKBOX"
+#define BB_LOG_NUM "BB LOG NUM" // stands for blackbox log number
+#define MAX_G_FORCE "MAX G-KRACHT"
+#define MAX_ESC_TEMP "MAX ESC TEMP"
+#define MAX_ESC_RPM "MAX ESC TPM"
+#define MIN_LINK "MIN LINK"
+#define PEAK_FFT "PEAK FFT"
+#define THRT_LESS_20 "THRT<20%" // short for throttle less than 20
+#define MIN_RSSI_DBM "MIN RSSI DBM"
+#define TOTAL_FLIGHTS "TOTAAL VLUCHTEN"
+#define TOTAL_FLIGHT_TIME "TOTALE VLIEG TIJD"
+#define TOTAL_DISTANCE "TOTALE AFSTAND"
+#define STATS "  --- STATS ---" // keep the length the same
+#define STATS "  -- CIJFERS --" // keep the length the same
+#define OSD_ARMED "ARMED (BEWAPEND)"
+// text from cms_menu_blackbox.c
+// text from cms_menu_failsafe.c
+// text from cms_menu_firmware.c
+// text from cms_menu_gps_rescue.c
+// text from cms_menu_imu.c
+// text from cms_menu_ledstrip.c
+// text from cms_menu_main.c
+// text from cms_menu_misc.c
+// text from cms_menu_osd.c
+// text from cms_menu_persistent_stats.c
+// text from cms_menu_power.c
+// text from cms_menu_saveexit.c
+// text from cms_menu_vtx_common.c
+// text from cms_menu_vtx_rtc6705.c
+// text from cms_menu_vtx_smartaudio.c
+// text from cms_menu_vtx_tramp.c
+// text from cms.c
 
 #elif defined(FRENCH_OSD)
 // text from osd_elements.c
