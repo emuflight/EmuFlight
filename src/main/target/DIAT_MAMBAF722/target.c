e51a7a86fce14437362659b81a955d5139b305c3
Kaiowarez <60309480+Kaiowarez@users.noreply.github.com>
2021-02-26 10:37:49 -0500

    name update

--- src/main/target/DIAT_MAMBAF722/target.c
+++ src/main/target/DIAT_MAMBAF722/target.c
@@ -36,10 +36,10 @@ const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
     DEF_TIM(TIM1,   CH1,  PA8,   TIM_USE_MOTOR, 0, 0 ), // S3_OUT – UP2-5
     DEF_TIM(TIM1,   CH2,  PA9,   TIM_USE_MOTOR, 0, 0 ), // S4_OUT – UP2-5
 
-    DEF_TIM(TIM3,   CH3,  PB0,   TIM_USE_MOTOR, 0, 0 ), // S4_OUT – UP2-5
-    DEF_TIM(TIM3,   CH4,  PB1,   TIM_USE_MOTOR, 0, 0 ), // S4_OUT – UP2-5
-    DEF_TIM(TIM1,   CH1,  PA10,   TIM_USE_MOTOR, 0, 0 ), // S4_OUT – UP2-5
-    DEF_TIM(TIM3,   CH1,  PB4,   TIM_USE_MOTOR, 0, 0 ), // S4_OUT – UP2-5
+    DEF_TIM(TIM3,   CH3,  PB0,   TIM_USE_MOTOR, 0, 0 ), // S5_OUT – UP2-5
+    DEF_TIM(TIM3,   CH4,  PB1,   TIM_USE_MOTOR, 0, 0 ), // S6_OUT – UP2-5
+    DEF_TIM(TIM1,   CH3,  PA10,  TIM_USE_MOTOR, 0, 0 ), // S7_OUT – UP2-5
+    DEF_TIM(TIM3,   CH1,  PB4,   TIM_USE_MOTOR, 0, 0 ), // S8_OUT – UP2-5
 
     DEF_TIM(TIM4,   CH3,  PB8,   TIM_USE_ANY,   0, 0 ), // FC CAM – DMA1_ST7
 
--- src/main/target/DIAT_MAMBAF722/target.h
+++ src/main/target/DIAT_MAMBAF722/target.h
@@ -188,5 +188,5 @@
 #define TARGET_IO_PORTC         0xffff
 #define TARGET_IO_PORTD         (BIT(2))
 
-#define USABLE_TIMER_CHANNEL_COUNT 7
-#define USED_TIMERS             ( TIM_N(1) | TIM_N(2) | TIM_N(4) | TIM_N(8) | TIM_N(11) )
+#define USABLE_TIMER_CHANNEL_COUNT 11
+#define USED_TIMERS             ( TIM_N(1) | TIM_N(2) | TIM_N(3) | TIM_N(4) | TIM_N(8) | TIM_N(11) )
