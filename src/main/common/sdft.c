9335c735af89a4f6e21d2dd50bfe1989e5a36075
Quick-Flash <46289813+Quick-Flash@users.noreply.github.com>
2021-02-27 19:42:55 -0700

    Revert "adding sdft part 2"
    This reverts commit 499553a1a0cd3c778789a24f6ef47a64eebe1cdb.

--- src/main/common/sdft.c
+++ src/main/common/sdft.c
@@ -31,13 +31,11 @@ static FAST_DATA_ZERO_INIT float     dampingFactor;
 static FAST_DATA_ZERO_INIT float     r_to_n;
 static FAST_DATA_ZERO_INIT complex_t twiddle[SDFT_BIN_COUNT];
 
-static void applySqrt(const sdft_t *sdft, float *data);
-
 
 void sdftInit(sdft_t *sdft, const uint8_t startBin, const uint8_t endBin)
 {
 	if (!constsInitialized) {
-		dampingFactor = nexttowardf(1.0f, 0.0f);
+		dampingFactor = nexttowardf(1, 0);
 		r_to_n = powf(dampingFactor, SDFT_SAMPLE_SIZE);
 		const float c = 2.0f * M_PIf / (float)SDFT_SAMPLE_SIZE;
 		float phi = 0.0f;
@@ -80,7 +78,7 @@ FAST_CODE void sdftMagSq(const sdft_t *sdft, float *output)
 {
 	float re;
 	float im;
-	for (uint8_t i = sdft->startBin; i < sdft->endBin; i++) {
+	for (uint8_t i = 0; i < SDFT_BIN_COUNT; i++) {
 		re = crealf(sdft->data[i]);
 		im = cimagf(sdft->data[i]);
 		output[i] = re * re + im * im;
@@ -88,51 +86,30 @@ FAST_CODE void sdftMagSq(const sdft_t *sdft, float *output)
 }
 
 
-// Get magnitude of frequency spectrum (slower)
-FAST_CODE void sdftMagnitude(const sdft_t *sdft, float *output)
+// Apply Hann window
+FAST_CODE void sdftWindow(const sdft_t *sdft, complex_t *output)
 {
-	sdftMagSq(sdft, output);
-	applySqrt(sdft, output);
+	for (uint8_t i = sdft->startBin; i < sdft->endBin; i++) {
+		output[i] = 0.5f * sdft->data[i] - 0.25f * sdft->data[i - 1] + sdft->data[i + 1];
+	}
+	output[sdft->endBin] = 0.5f * sdft->data[sdft->endBin] - 0.25f * sdft->data[sdft->endBin - 1] + sdft->data[(sdft->endBin + 1) % SDFT_BIN_COUNT];
 }
 
 
-// Get quared magnitude of frequency spectrum with Hann window applied
-FAST_CODE void sdftWinSq(const sdft_t *sdft, float *output)
+// Apply Hann window and calculate squared magnitude
+FAST_CODE void sdftWindowf(const sdft_t *sdft, float *output)
 {
-	complex_t val;
+	complex_t temp;
 	float re;
 	float im;
-
-	val = 0.5f * sdft->data[sdft->startBin] - 0.25f * sdft->data[sdft->endBin] + sdft->data[(sdft->startBin + 1)];
-	re = crealf(val);
-	im = cimagf(val);
-	output[sdft->startBin] = re * re + im * im;
-
-	for (uint8_t i = (sdft->startBin + 1); i < sdft->endBin; i++) {
-		val = 0.5f * sdft->data[i] - 0.25f * sdft->data[i - 1] + sdft->data[i + 1];
-		re = crealf(val);
-		im = cimagf(val);
+	for (uint8_t i = sdft->startBin; i < sdft->endBin; i++) {
+		temp = 0.5f * sdft->data[i] - 0.25f * sdft->data[i - 1] + sdft->data[i + 1];
+		re = crealf(temp);
+		im = cimagf(temp);
 		output[i] = re * re + im * im;
 	}
-
-	val = 0.5f * sdft->data[sdft->endBin] - 0.25f * sdft->data[sdft->endBin - 1] + sdft->data[(sdft->startBin)];
-	re = crealf(val);
-	im = cimagf(val);
+	temp = 0.5f * sdft->data[sdft->endBin] - 0.25f * sdft->data[sdft->endBin - 1] + sdft->data[(sdft->endBin + 1) % SDFT_BIN_COUNT];
+	re = crealf(temp);
+	im = cimagf(temp);
 	output[sdft->endBin] = re * re + im * im;
-}
-
-
-// Get magnitude of frequency spectrum with Hann window applied (slower)
-FAST_CODE void sdftWindow(const sdft_t *sdft, float *output)
-{
-	sdftWinSq(sdft, output);
-	applySqrt(sdft, output);
-}
-
-
-static FAST_CODE void applySqrt(const sdft_t *sdft, float *data)
-{
-	for (uint8_t i = sdft->startBin; i > sdft->endBin; i++) {
-		data[i] = sqrt_approx(data[i]);
-	}
 }
