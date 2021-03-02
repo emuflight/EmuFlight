17911b5ac584362740170bea4fe3edcc5ac73718
Quick-Flash <46289813+Quick-Flash@users.noreply.github.com>
2021-03-01 23:35:46 -0700

    revert custom defaults

--- src/main/target/STM32_UNIFIED/target.h
+++ src/main/target/STM32_UNIFIED/target.h
@@ -248,8 +248,6 @@
 #define USE_RX_SPEKTRUM
 #define USE_RX_SPEKTRUM_TELEMETRY
 
-//#define USE_CUSTOM_DEFAULTS
-
 // Additional drivers included for targets with > 512KB of flash
 #if (TARGET_FLASH_SIZE > 512)
 #define USE_ACC_MPU6050
