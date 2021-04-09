a35d827be34d8c5797130462adfefbeb1c1d8693
madchiller <DJ.madchiller@gmail.com>
2021-04-09 19:59:34 -0400

    LED TIMER Adjust

--- src/main/target/TBSPODRACERAIO/target.c
+++ src/main/target/TBSPODRACERAIO/target.c
@@ -35,6 +35,6 @@ const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
     DEF_TIM(TIM4,   CH1,  PB6,  TIM_USE_MOTOR, 0, 0 ), // S3_OUT - DMA1_ST0_CH2
     DEF_TIM(TIM4,   CH2,  PB7,  TIM_USE_MOTOR, 0, 0 ), // S4_OUT - DMA1_ST3_CH2
 
-    DEF_TIM(TIM1,   CH1,  PA8,  TIM_USE_ANY,   0, 0 ), // LED_STRIP - DMA2_ST3_CH6
+    DEF_TIM(TIM1,   CH1,  PA8,  TIM_USE_LED,   0, 0 ), // LED_STRIP - DMA2_ST3_CH6
     DEF_TIM(TIM1,   CH2,  PA9,  TIM_USE_ANY,   0, 0 ), // Softserial
 };
