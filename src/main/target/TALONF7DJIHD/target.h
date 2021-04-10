7cc4be5a4d29d71e567e8c1c90bddd54436314d2
madchiller <DJ.madchiller@gmail.com>
2021-04-10 17:39:56 -0400

    Adding missing Timers
    Adding timers for TX/RX/CC/MOSI ect..

--- src/main/target/TALONF7DJIHD/target.h
+++ src/main/target/TALONF7DJIHD/target.h
@@ -144,5 +144,5 @@
 #define TARGET_IO_PORTD                                 (BIT(2))
 
 // timers
-#define USABLE_TIMER_CHANNEL_COUNT                      9                       //updated timer count to compensate for Nf Motor 4
+#define USABLE_TIMER_CHANNEL_COUNT                      15                       //updated timer count to compensate for Nf Motor 4
 #define USED_TIMERS ( TIM_N(1) |TIM_N(2) | TIM_N(3) | TIM_N(4) | TIM_N(5)  | TIM_N(8) | TIM_N(9) ) //update based on update CLRACINGF7 Target BF4.1+
