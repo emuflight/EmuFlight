0e82779c4c826af9d959910259071dbb82081228
madchiller <DJ.madchiller@gmail.com>
2021-04-10 17:57:53 -0400

    Clean UP

--- src/main/target/TALONF7DJIHD/target.c
+++ src/main/target/TALONF7DJIHD/target.c
@@ -34,18 +34,18 @@ const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
     DEF_TIM(TIM4,  CH2,  PB7, TIM_USE_MOTOR,               0, 0), // D1-ST3                   MOTOR2
     DEF_TIM(TIM4,  CH3,  PB8, TIM_USE_MOTOR,               0, 0), // D1-ST7                   MOTOR3
     DEF_TIM(TIM4,  CH4,  PB9, TIM_USE_MOTOR,               0, 0), // D2-ST2/D2-ST4            MOTOR4
-    DEF_TIM(TIM5,  CH2,  PA1, TIM_USE_MOTOR | TIM_USE_ANY,  0, 0), // D1-ST4                  MOTOR5/RX4
+    DEF_TIM(TIM5,  CH2,  PA1, TIM_USE_MOTOR | TIM_USE_ANY, 0, 0), // D1-ST4                  MOTOR5/RX4
     DEF_TIM(TIM8,  CH3,  PC8, TIM_USE_MOTOR,               0, 0), // NONE  TIM4_UP_D1-ST6     MOTOR6
     DEF_TIM(TIM8,  CH4,  PC9, TIM_USE_MOTOR,               0, 0), // D2-ST7                   MOTOR7
 
-    DEF_TIM(TIM3,  CH4,  PB1, TIM_USE_MOTOR | TIM_USE_LED,  0, 0), // D1-ST2                   LED/MOTOR8
+    DEF_TIM(TIM3,  CH4,  PB1, TIM_USE_MOTOR | TIM_USE_LED, 0, 0), // D1-ST2                   LED/MOTOR8
 
-    DEF_TIM(TIM1,  CH2N,  PB0, TIM_USE_ANY,                 0, 0), // LED 1
+    DEF_TIM(TIM1,  CH2N, PB0, TIM_USE_ANY,                 0, 0), // LED 1
     DEF_TIM(TIM5,  CH1,  PA0, TIM_USE_ANY,                 0, 0), // TX4
     DEF_TIM(TIM3,  CH1,  PC6, TIM_USE_ANY,                 0, 0), // TX6
     DEF_TIM(TIM8,  CH2,  PC7, TIM_USE_ANY,                 0, 0), // RX6
     DEF_TIM(TIM3,  CH2,  PB5, TIM_USE_ANY,                 0, 0), //SPI_MOSI 3
-    DEF_TIM(TIM2,  CH2,  PB3, TIM_USE_ANY,                 0, 0), // Camera Control 1
+    DEF_TIM(TIM2,  CH2,  PB3, TIM_USE_ANY,                 0, 0) // Camera Control 1
 
-    
+
 };
