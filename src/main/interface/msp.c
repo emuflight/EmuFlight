f173450055b8bb9a1f3a566ada203e682a915f58
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2021-05-08 11:28:30 -0500

    resolve #555; DJIwarning only if OSD defined
    * fixes for FC without OSD

--- src/main/interface/msp.c
+++ src/main/interface/msp.c
@@ -768,10 +768,12 @@ bool mspProcessOutCommand(uint8_t cmdMSP, sbuf_t *dst) {
     case MSP_NAME: {
         // Show warning for DJI OSD instead of pilot name
         // works if osd warnings enabled, osd_warn_dji is on and usb is not connected
+        #ifdef USE_OSD
         if (osdWarnGetState(OSD_WARNING_DJI)) {
             sbufWriteString(dst, djiWarningBuffer);
             break;
         }
+        #endif
         const int nameLen = strlen(pilotConfig()->name);
         for (int i = 0; i < nameLen; i++) {
             sbufWriteU8(dst, pilotConfig()->name[i]);
