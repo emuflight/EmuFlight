efc1ac452979d72f99fdfbf58e61a18da573a3d6
Kevin Plaizier <46289813+Quick-Flash@users.noreply.github.com>
2021-06-01 14:53:10 -0600

    Smith-Predictor on/off toggle [smith_predict_enabled] (#583)
    * Update gyro.h
    * Update gyro.h
    * Update gyro.c
    * Update settings.c
    * Update cms_menu_imu.c, do we want to move the location of the smith stuff?
    * Update settings.c
    * Update cms_menu_imu.c
    * smith predictor toggle, fix type-o
    * Update settings.c
    * Update cms_menu_imu.c
    * SmithPredictor enable toggle
    * SmithPredictor enable toggle, we overlapping work, fix variable names
    * SmithPredictor enable toggle, we overlapping work, fix variable names
    * Update cms_menu_imu.c
    * Smith-Predictor toggle
    * ON/OFF
    * SmithPredictor Toggle
    * BBL header for smith_predict_enabled
    
    Co-authored-by: nerdCopter <56646290+nerdCopter@users.noreply.github.com>

--- src/main/cms/cms_menu_imu.c
+++ src/main/cms/cms_menu_imu.c
@@ -505,6 +505,7 @@ static uint16_t gyroConfig_imuf_w;
 static uint16_t gyroConfig_imuf_sharpness;
 #endif
 #ifdef USE_SMITH_PREDICTOR
+static uint8_t smithPredictor_enabled;
 static uint8_t smithPredictor_strength;
 static uint8_t smithPredictor_delay;
 static uint16_t smithPredictor_filt_hz;
@@ -536,6 +537,7 @@ static long cmsx_menuGyro_onEnter(void) {
     gyroConfig_imuf_sharpness = gyroConfig()->imuf_sharpness;
 #endif
 #ifdef USE_SMITH_PREDICTOR
+    smithPredictor_enabled   = gyroConfig()->smithPredictorEnabled;
     smithPredictor_strength  = gyroConfig()->smithPredictorStrength;
     smithPredictor_delay     = gyroConfig()->smithPredictorDelay;
     smithPredictor_filt_hz   = gyroConfig()->smithPredictorFilterHz;
@@ -569,6 +571,7 @@ static long cmsx_menuGyro_onExit(const OSD_Entry *self) {
     gyroConfigMutable()->imuf_sharpness = gyroConfig_imuf_sharpness;
 #endif
 #ifdef USE_SMITH_PREDICTOR
+    gyroConfigMutable()->smithPredictorEnabled = smithPredictor_enabled;
     gyroConfigMutable()->smithPredictorStrength = smithPredictor_strength;
     gyroConfigMutable()->smithPredictorDelay = smithPredictor_delay;
     gyroConfigMutable()->smithPredictorFilterHz = smithPredictor_filt_hz;
@@ -608,9 +611,10 @@ static OSD_Entry cmsx_menuFilterGlobalEntries[] = {
     { "GYRO ABG HL",      OME_UINT8,  NULL, &(OSD_UINT8_t)  { &gyroConfig_gyro_abg_half_life,       0, 250, 1 }, 0 },
 
 #ifdef USE_SMITH_PREDICTOR
+    { "SMITH ENABLED",   OME_TAB,    NULL, &(OSD_TAB_t)    { (uint8_t *) &smithPredictor_enabled, 1, cms_offOnLabels }, 0 },
     { "SMITH STR",       OME_UINT8,  NULL, &(OSD_UINT8_t)  { &smithPredictor_strength,    0, 100, 1 }, 0 },
     { "SMITH DELAY",     OME_UINT8,  NULL, &(OSD_UINT8_t)  { &smithPredictor_delay,       0, 120, 1 }, 0 },
-    { "SMITH FILT",      OME_UINT16,  NULL, &(OSD_UINT16_t)  { &smithPredictor_filt_hz,   1, 1000, 1 }, 0 },
+    { "SMITH FILT",      OME_UINT16, NULL, &(OSD_UINT16_t) { &smithPredictor_filt_hz,   1, 1000, 1 }, 0 },
 #endif
 
     { "SAVE&EXIT",   OME_OSD_Exit, cmsMenuExit,   (void *)CMS_EXIT_SAVE, 0},
