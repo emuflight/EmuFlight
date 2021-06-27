ac7fecc33439370d1dc7247cb4963e5c5ef3db01
Quick-Flash <46289813+Quick-Flash@users.noreply.github.com>
2021-06-26 22:49:08 -0600

    Update gyro_filter_impl.h

--- src/main/sensors/gyro_filter_impl.h
+++ src/main/sensors/gyro_filter_impl.h
@@ -34,7 +34,7 @@ static FAST_CODE void GYRO_FILTER_FUNCTION_NAME(gyroSensor_t *gyroSensor) {
 #ifndef USE_GYRO_IMUF9001
         gyroADCf = kalman_update(gyroADCf, axis);
 #endif
-        
+
         // apply static notch filters and software lowpass filters
         gyroADCf = gyroSensor->lowpass2FilterApplyFn((filter_t *)&gyroSensor->lowpass2Filter[axis], gyroADCf);
         gyroADCf = gyroSensor->lowpassFilterApplyFn((filter_t *)&gyroSensor->lowpassFilter[axis], gyroADCf);
@@ -56,8 +56,10 @@ static FAST_CODE void GYRO_FILTER_FUNCTION_NAME(gyroSensor_t *gyroSensor) {
             }
         }
 #endif
-        update_kalman_covariance(gyroADCf, axis);
 
+#ifndef USE_GYRO_IMUF9001
+        update_kalman_covariance(gyroADCf, axis);
+#endif
         applySmithPredictor(gyroSensor->smithPredictor, gyroADCf);
 
 #ifdef USE_GYRO_IMUF9001
