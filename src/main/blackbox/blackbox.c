03aa735484aba0c51701a1328a6533c60fde7241
AndreySemjonov <50518855+AndreySemjonov@users.noreply.github.com>
2021-07-03 03:17:55 +0300

    Update blackbox.c

--- src/main/blackbox/blackbox.c
+++ src/main/blackbox/blackbox.c
@@ -1090,6 +1090,7 @@ static bool blackboxWriteSysinfo(void) {
     switch (xmitState.headerIndex) {
         BLACKBOX_PRINT_HEADER_LINE("Firmware type", "%s",                   "Cleanflight");
         BLACKBOX_PRINT_HEADER_LINE("Firmware revision", "%s %s (%s) %s",    FC_FIRMWARE_NAME, "3.7.0", shortGitRevision, targetName);
+        BLACKBOX_PRINT_HEADER_LINE("Target", "%s",                          targetName);
         BLACKBOX_PRINT_HEADER_LINE("Firmware date", "%s %s",                buildDate, buildTime);
         BLACKBOX_PRINT_HEADER_LINE("Log start datetime", "%s",              blackboxGetStartDateTime(buf));
         BLACKBOX_PRINT_HEADER_LINE("Craft name", "%s",                      pilotConfig()->name);
