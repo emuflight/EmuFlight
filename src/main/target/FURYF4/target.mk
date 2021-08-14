f150725b3bc4bd9dd477a35983056579649bd127
JuliooCesarMDM <juliomatias123@outlook.com>
2021-08-14 00:11:33 -0300

    fix3

--- src/main/target/FURYF4/target.mk
+++ src/main/target/FURYF4/target.mk
@@ -13,11 +13,8 @@ TARGET_SRC = \
 
 ifeq ($(TARGET), FURYF4OSD)
 TARGET_SRC += \
-            drivers/max7456.c \
-            drivers/barometer/barometer_ms5611.c \
-            drivers/barometer/barometer_bmp280.c 
-else
+            drivers/max7456.c
+endif
 TARGET_SRC += \
             drivers/barometer/barometer_ms5611.c \
             drivers/barometer/barometer_bmp280.c 
-endif
