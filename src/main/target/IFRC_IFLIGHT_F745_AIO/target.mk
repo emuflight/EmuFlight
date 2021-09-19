ae846550342b0de50c2f63c15f4fe75443bd44ae
JuliooCesarMDM <juliomatias123@outlook.com>
2021-09-19 13:57:02 -0300

    iflight f745

--- src/main/target/IFRC_IFLIGHT_F745_AIO/target.mk
+++ src/main/target/IFRC_IFLIGHT_F745_AIO/target.mk
@@ -5,6 +5,7 @@ TARGET_SRC = \
             drivers/accgyro/accgyro_spi_mpu6000.c \
             drivers/barometer/barometer_ms5611.c \
             drivers/barometer/barometer_bmp280.c \
+            drivers/barometer/barometer_dps310.c \
             drivers/compass/compass_hmc5883l.c \
             drivers/compass/compass_qmc5883l.c \
             drivers/light_ws2811strip.c \
