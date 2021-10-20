e2a1d071dcd7a759fb41c6ade36aba4230084bb0
BeauBrewski <85623381+BeauBrewski@users.noreply.github.com>
2021-10-20 09:39:24 -0500

    FLYWOOF745

---
+++ src/main/target/FLYWOOF745/target.mk
@@ -0,0 +1,12 @@
+F7X5XG_TARGETS += $(TARGET)
+FEATURES       += VCP ONBOARDFLASH
+
+TARGET_SRC = \
+            drivers/accgyro/accgyro_spi_mpu6000.c \
+            drivers/barometer/barometer_bmp280.c \
+            drivers/compass/compass_hmc5883l.c \
+            drivers/compass/compass_qmc5883l.c \
+					  drivers/compass/compass_lis3mdl.c \
+            drivers/light_ws2811strip.c \
+            drivers/light_ws2811strip_hal.c \
+            drivers/max7456.c
