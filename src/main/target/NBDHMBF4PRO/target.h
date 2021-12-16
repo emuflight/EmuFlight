ba0a68c417fed5674c0b40088947278845d358aa
BeauBrewski <85623381+BeauBrewski@users.noreply.github.com>
2021-12-16 02:46:57 -0600

    Add redpine to NBDHMBF4PRO (#741)
    Co-authored-by: nerdCopter <56646290+nerdCopter@users.noreply.github.com>

--- src/main/target/NBDHMBF4PRO/target.h
+++ src/main/target/NBDHMBF4PRO/target.h
@@ -94,6 +94,7 @@
 #define USE_RX_FRSKY_SPI_D
 #define USE_RX_FRSKY_SPI_X
 #define USE_RX_SFHSS_SPI
+#define USE_RX_REDPINE_SPI
 
 // *************** OSD *****************************
 #define USE_MAX7456
