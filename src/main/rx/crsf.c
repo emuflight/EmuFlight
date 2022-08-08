d50d816d0f6631c5a12527cc23f105f06824b4a8
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2022-08-08 08:16:29 -0500

    radix - deduplicate crsf telemetry variables

--- src/main/rx/crsf.c
+++ src/main/rx/crsf.c
@@ -119,19 +119,6 @@ struct crsfPayloadRcChannelsPacked_s {
 
 typedef struct crsfPayloadRcChannelsPacked_s crsfPayloadRcChannelsPacked_t;
 
-struct crsfPayloadLinkStatistics_s {
-    uint8_t     uplinkRSSIAnt1;
-    uint8_t     uplinkRSSIAnt2;
-    uint8_t     uplinkLQ;
-    int8_t      uplinkSNR;
-    uint8_t     activeAntenna;
-    uint8_t     rfMode;
-    uint8_t     uplinkTXPower;
-    uint8_t     downlinkRSSI;
-    uint8_t     downlinkLQ;
-    int8_t      downlinkSNR;
-} __attribute__ ((__packed__));
-
 typedef struct crsfPayloadLinkstatistics_s {
     uint8_t uplink_RSSI_1;
     uint8_t uplink_RSSI_2;
