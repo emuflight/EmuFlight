69805abaffb877c27b426fdd14a117ebb068a7e8
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2022-08-11 14:29:38 -0500

    bmi270 - align ifrcf745aiov2 target

--- src/main/target/IFLIGHT_F745_AIO_V2/target.mk
+++ src/main/target/IFLIGHT_F745_AIO_V2/target.mk
@@ -2,6 +2,7 @@ F7X5XG_TARGETS += $(TARGET)
 FEATURES       += VCP ONBOARDFLASH
 
 TARGET_SRC = \
+            $(ROOT)/lib/main/BoschSensortec/BMI270-Sensor-API/bmi270_maximum_fifo.c \
             drivers/accgyro/accgyro_spi_bmi270.c \
             drivers/barometer/barometer_ms5611.c \
             drivers/barometer/barometer_bmp280.c \
