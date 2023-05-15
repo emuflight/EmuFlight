639c8e36133a35361da2605f5dd69d51d4dd5857
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2023-05-15 12:57:11 -0500

    unneeded files cleanup and file move; target path change

--- src/main/target/TMTR_TMOTORF7/target.mk
+++ src/main/target/TMTR_TMOTORF7/target.mk
@@ -1,6 +1,8 @@
 F7X2RE_TARGETS   += $(TARGET)
 FEATURES       = VCP ONBOARDFLASH
 TARGET_SRC = \
+            drivers/accgyro/bmi270_maximum_fifo.c \
+            drivers/accgyro/accgyro_spi_bmi270.c \
             drivers/accgyro/accgyro_mpu6500.c \
             drivers/accgyro/accgyro_spi_mpu6000.c \
             drivers/accgyro/accgyro_spi_mpu6500.c \
