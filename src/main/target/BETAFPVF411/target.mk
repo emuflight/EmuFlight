578c743feb9d4d9e97dc70602c93e49a35eaf135
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2023-05-15 13:57:38 -0500

    bmi270 - more file cleanup

--- src/main/target/BETAFPVF411/target.mk
+++ src/main/target/BETAFPVF411/target.mk
@@ -4,7 +4,7 @@ FEATURES       = VCP ONBOARDFLASH
 TARGET_SRC = \
             drivers/accgyro/accgyro_mpu.c \
             drivers/accgyro/accgyro_spi_mpu6000.c \
-            drivers/accgyro/bmi270_maximum_fifo.c \
+            drivers/accgyro/accgyro_bmi270_maximum_fifo.c \
             drivers/accgyro/accgyro_spi_bmi270.c \
             drivers/barometer/barometer_bmp085.c \
             drivers/barometer/barometer_bmp280.c \
