26b4b03b938a7a707e001572a52fb3e530a227e1
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2023-05-15 13:57:38 -0500

    bmi270 - more file cleanup

--- src/main/target/IFLIGHT_F745_AIO_V2/target.mk
+++ src/main/target/IFLIGHT_F745_AIO_V2/target.mk
@@ -2,7 +2,7 @@ F7X5XG_TARGETS += $(TARGET)
 FEATURES       += VCP ONBOARDFLASH
 
 TARGET_SRC = \
-            drivers/accgyro/bmi270_maximum_fifo.c \
+            drivers/accgyro/accgyro_bmi270_maximum_fifo.c \
             drivers/accgyro/accgyro_spi_bmi270.c \
             drivers/barometer/barometer_ms5611.c \
             drivers/barometer/barometer_bmp280.c \
