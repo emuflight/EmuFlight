067bc1927766b6df725f0e520b073257605372a4
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2023-06-06 11:29:36 -0500

    BMI270 - normal mode drivers per target

--- src/main/target/NBD_INFINITYAIOV2PRO/target.mk
+++ src/main/target/NBD_INFINITYAIOV2PRO/target.mk
@@ -2,14 +2,13 @@ F7X5XG_TARGETS += $(TARGET)
 FEATURES       += VCP ONBOARDFLASH
 
 TARGET_SRC = \
-            drivers/accgyro/accgyro_bmi270_maximum_fifo.c \
+            drivers/accgyro/accgyro_bmi270.c \
             drivers/accgyro/accgyro_spi_bmi270.c \
             drivers/light_ws2811strip.c \
             drivers/max7456.c \
-drivers/compass/compass_fake.c \
-drivers/compass/compass_ak8963.c \
-drivers/compass/compass_ak8975.c \
-drivers/compass/compass_hmc5883l.c \
-drivers/compass/compass_lis3mdl.c \
-drivers/compass/compass_qmc5883l.c \
-
+            drivers/compass/compass_fake.c \
+            drivers/compass/compass_ak8963.c \
+            drivers/compass/compass_ak8975.c \
+            drivers/compass/compass_hmc5883l.c \
+            drivers/compass/compass_lis3mdl.c \
+            drivers/compass/compass_qmc5883l.c \
--- src/main/target/SKYSTARSF7HDPRO/target.mk
+++ src/main/target/SKYSTARSF7HDPRO/target.mk
@@ -2,16 +2,16 @@ F7X2RE_TARGETS  += $(TARGET)
 FEATURES        += VCP ONBOARDFLASH
 
 TARGET_SRC = \
-						drivers/accgyro/accgyro_bmi270_maximum_fifo.c \
-						drivers/accgyro/accgyro_spi_bmi270.c \
-						drivers/accgyro/accgyro_spi_mpu6000.c \
+            drivers/accgyro/accgyro_bmi270.c \
+            drivers/accgyro/accgyro_spi_bmi270.c \
+            drivers/accgyro/accgyro_spi_mpu6000.c \
             drivers/accgyro/accgyro_mpu.c \
-						drivers/accgyro/accgyro_mpu6500.c \
-						drivers/accgyro/accgyro_spi_mpu6500.c \
+            drivers/accgyro/accgyro_mpu6500.c \
+            drivers/accgyro/accgyro_spi_mpu6500.c \
             drivers/barometer/barometer_bmp280.c \
-          	drivers/barometer/barometer_ms5611.c \
-						drivers/barometer/barometer_bmp085.c \
+            drivers/barometer/barometer_ms5611.c \
+            drivers/barometer/barometer_bmp085.c \
             drivers/compass/compass_hmc5883l.c \
             drivers/compass/compass_qmc5883l.c \
-						drivers/light_ws2811strip.c \
+            drivers/light_ws2811strip.c \
             drivers/max7456.c
