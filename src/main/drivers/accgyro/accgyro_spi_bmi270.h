ab236b7e03165c909129277755cbd234beb72ada
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2023-06-16 08:44:27 -0500

    BMI270 - modify GPL License

--- src/main/drivers/accgyro/accgyro_spi_bmi270.h
+++ src/main/drivers/accgyro/accgyro_spi_bmi270.h
@@ -1,19 +1,21 @@
 /*
- * This file is part of Cleanflight and Betaflight.
+ * This file is part of EmuFlight. It was ported from 
+ * Betaflight/Betaflight and FlightNG/ATBetaflight.
  *
- * Cleanflight and Betaflight are free software. You can redistribute
- * this software and/or modify this software under the terms of the
- * GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * This is free software. You can redistribute this software
+ * and/or modify this software under the terms of the GNU General
+ * Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later
+ * version.
  *
- * Cleanflight and Betaflight are distributed in the hope that they
- * will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
  * See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this software.
+ * You should have received a copy of the GNU General Public
+ * License along with this software.
  *
  * If not, see <http://www.gnu.org/licenses/>.
  */
