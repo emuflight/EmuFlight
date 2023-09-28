e4fa92065b8103ae094dbf196fe10fbd59bf11db
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2023-09-28 09:29:48 -0500

    BMI270 - sync targets

--- src/main/target/FLYWOOF411_5IN1_AIO/target.mk
+++ src/main/target/FLYWOOF411_5IN1_AIO/target.mk
@@ -4,10 +4,10 @@ FEATURES        += VCP
 
 TARGET_SRC = \
             drivers/accgyro/accgyro_mpu.c \
-            drivers/accgyro/accgyro_spi_mpu6000.c \
+            drivers/accgyro/accgyro_spi_bmi270.c \
             drivers/accgyro/accgyro_spi_icm20689.c \
             drivers/accgyro/accgyro_spi_icm426xx.c \
-            drivers/accgyro/accgyro_spi_bmi270.c \
+            drivers/accgyro/accgyro_spi_mpu6000.c \
             drivers/barometer/barometer_bmp085.c \
             drivers/barometer/barometer_bmp280.c \
             drivers/barometer/barometer_ms5611.c \
