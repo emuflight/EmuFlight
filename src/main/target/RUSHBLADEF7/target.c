75b413e75400739eacd5f51b6a012c6896e5a573
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2023-12-18 10:04:43 -0600

    [target] RUSH BLADEF7 update/repair (#957)

--- src/main/target/RUSHBLADEF7/target.c
+++ src/main/target/RUSHBLADEF7/target.c
@@ -1,41 +1,45 @@
 /*
- * This file is part of Cleanflight and Betaflight.
+ * This file is part of EmuFlight. It is derived from Betaflight.
  *
- * Cleanflight and Betaflight are free software. You can redistribute
- * this software and/or modify this software under the terms of the
- * GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * This is free software. You can redistribute this software
+ * and/or modify this software under the terms of the GNU General
+ * Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later
+ * version.
  *
- * Cleanflight and Betaflight are distributed in the hope that they
- * will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
  * See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this software.
+ * You should have received a copy of the GNU General Public
+ * License along with this software.
  *
  * If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <stdint.h>
+// This resource file generated using https://github.com/nerdCopter/target-convert
+// Commit: c4eda1b
 
+#include <stdint.h>
 #include "platform.h"
 #include "drivers/io.h"
-
 #include "drivers/dma.h"
 #include "drivers/timer.h"
 #include "drivers/timer_def.h"
 
 const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
-    DEF_TIM(TIM2, CH1, PA15,  TIM_USE_LED,   0, 0), // PPM&SBUS
-
-    DEF_TIM(TIM8, CH3, PC8,  TIM_USE_MOTOR, 0, 0),   // S1
-    DEF_TIM(TIM8, CH4, PC9,  TIM_USE_MOTOR, 0, 0),   // S2
-    DEF_TIM(TIM3, CH4, PB1,  TIM_USE_MOTOR, 0, 0),   // S3
-    DEF_TIM(TIM3, CH3, PB0,  TIM_USE_MOTOR, 0, 0),   // S4
-    DEF_TIM(TIM8, CH2, PC7,  TIM_USE_MOTOR, 0, 0),   // S5
-    DEF_TIM(TIM8, CH1, PC6,  TIM_USE_MOTOR, 0, 0),   // S6
-
-    DEF_TIM(TIM1, CH1, PA8,  TIM_USE_ANY, 0, 0),    // LED STRIP
+    DEF_TIM(TIM8, CH3, PC8, TIM_USE_MOTOR, 0, 0), // motor 1
+    DEF_TIM(TIM8, CH4, PC9, TIM_USE_MOTOR, 0, 0), // motor 2
+    DEF_TIM(TIM3, CH4, PB1, TIM_USE_MOTOR, 0, 0), // motor 3
+    DEF_TIM(TIM3, CH3, PB0, TIM_USE_MOTOR, 0, 0), // motor 4
+    DEF_TIM(TIM8, CH2, PC7, TIM_USE_MOTOR, 0, 0), // motor 5
+    DEF_TIM(TIM8, CH1, PC6, TIM_USE_MOTOR, 0, 0), // motor 6
+    DEF_TIM(TIM4, CH1, PB6, TIM_USE_MOTOR, 0, 0), // motor 7
+    DEF_TIM(TIM4, CH2, PB7, TIM_USE_MOTOR, 0, 0), // motor 8
+    DEF_TIM(TIM2, CH1, PA15, TIM_USE_LED, 0, 0), // led
+    DEF_TIM(TIM1, CH1, PA8, TIM_USE_ANY, 0, 0), // cam ctrl
 };
+
+// notice - this file was programmatically generated and may be incomplete.
