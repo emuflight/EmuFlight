e796ee187abc2eb59540070ec87dc54889a54dc3
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2024-03-21 13:50:41 -0500

    [target] PYRODRONEF7 - trim trailing spaces

--- src/main/target/PYRODRONEF7/target.c
+++ src/main/target/PYRODRONEF7/target.c
@@ -28,7 +28,7 @@
 
 const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
 // FILO arrangement for motor assignments, Motor 1 starts at 2nd DECLARATION
-    DEF_TIM(TIM2,  CH2,   PB3, TIM_USE_ANY,                0, 0), // pin B03: TIM2 CH2 (AF1) // USE FOR CAMERA CONTROL 
+    DEF_TIM(TIM2,  CH2,   PB3, TIM_USE_ANY,                0, 0), // pin B03: TIM2 CH2 (AF1) // USE FOR CAMERA CONTROL
 
     DEF_TIM(TIM4,  CH1,  PB6, TIM_USE_MOTOR,               0, 0), // pin B06: TIM4 CH1 (AF2) // D1-ST0                   MOTOR1
     DEF_TIM(TIM4,  CH2,  PB7, TIM_USE_MOTOR,               0, 0), // pin B07: TIM4 CH2 (AF2) // D1-ST3                   MOTOR2
@@ -38,7 +38,7 @@ const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
     DEF_TIM(TIM4,  CH4,  PB9, TIM_USE_MOTOR,               0, 0), // pin B09: TIM4 CH4 (AF2) // NONE  TIM4_UP_D1-ST6     MOTOR6
     DEF_TIM(TIM8,  CH4,  PC9, TIM_USE_MOTOR,               0, 0), // pin C09: TIM8 CH4 (AF3) // D2-ST7                   MOTOR7
 
-    DEF_TIM(TIM3,  CH4, PB1, TIM_USE_MOTOR | TIM_USE_LED,  0, 0), // pin B01: TIM3 CH4 (AF2) // D1-ST2                   LED/MOTOR5 
+    DEF_TIM(TIM3,  CH4, PB1, TIM_USE_MOTOR | TIM_USE_LED,  0, 0), // pin B01: TIM3 CH4 (AF2) // D1-ST2                   LED/MOTOR5
 
     DEF_TIM(TIM9,  CH2,  PA3, TIM_USE_ANY,                 0, 0), // pin A03: TIM9 CH2 (AF3)
     DEF_TIM(TIM1,  CH2N, PB0, TIM_USE_ANY,                 0, 0), // pin B00: TIM1 CH2N (AF1)
