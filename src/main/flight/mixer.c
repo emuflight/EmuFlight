61974fe3679580c7ca552969d2358045d480dd88
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2024-03-28 10:04:20 -0500

    default pid denom 2 only for F411 (and unused F3)

--- src/main/flight/mixer.c
+++ src/main/flight/mixer.c
@@ -100,7 +100,11 @@ void pgResetFn_motorConfig(motorConfig_t *motorConfig) {
     {
         motorConfig->minthrottle = 1070;
         motorConfig->dev.motorPwmRate = BRUSHLESS_MOTORS_PWM_RATE;
+#if defined(STM32F411xE)
+        motorConfig->dev.motorPwmProtocol = PWM_TYPE_DSHOT300;
+#else
         motorConfig->dev.motorPwmProtocol = PWM_TYPE_DSHOT600;
+#endif
     }
 #endif
     motorConfig->maxthrottle = 2000;
