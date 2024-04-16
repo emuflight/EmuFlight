6a027e886e7cea3c9be42e1b0a79d9f479696ed4
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2024-04-16 15:31:44 -0500

    [target] ALIENFLIGHTF4 add MPU9250, update id (#1006)

--- src/main/target/ALIENFLIGHTF4/target.mk
+++ src/main/target/ALIENFLIGHTF4/target.mk
@@ -2,8 +2,10 @@ F405_TARGETS    += $(TARGET)
 FEATURES        += SDCARD VCP ONBOARDFLASH
 
 TARGET_SRC = \
+            drivers/accgyro/accgyro_mpu.c \
             drivers/accgyro/accgyro_mpu6500.c \
             drivers/accgyro/accgyro_spi_mpu6500.c \
+            drivers/accgyro/accgyro_spi_mpu9250.c \
             drivers/barometer/barometer_bmp280.c \
             drivers/barometer/barometer_ms5611.c \
             drivers/compass/compass_ak8963.c \
