3cb9bb5f9e2bb508774c1fa76832f81a5f580441
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2024-04-18 15:04:55 -0500

    [target] add TMOTORF411

---
+++ src/main/target/TMOTORF411/target.mk
@@ -0,0 +1,12 @@
+F411_TARGETS   += $(TARGET)
+FEATURES       += VCP ONBOARDFLASH
+
+TARGET_SRC = \
+drivers/accgyro/accgyro_spi_mpu6000.c \
+drivers/accgyro/accgyro_spi_bmi270.c \
+drivers/light_led.h \
+drivers/light_ws2811strip.c \
+drivers/max7456.c \
+
+# This resource file generated using https://github.com/nerdCopter/target-convert
+# Commit: 889ce5d
