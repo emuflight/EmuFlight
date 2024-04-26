0ab8d01f4ddcfd2da65b6470a814826cdfc8fdc4
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2024-04-26 11:43:47 -0500

    [target] fix FLYWOOF7DUAL timer count (#1052)

--- src/main/target/FLYWOOF7DUAL/target.h
+++ src/main/target/FLYWOOF7DUAL/target.h
@@ -179,5 +179,5 @@
 #define TARGET_IO_PORTC         0xffff
 #define TARGET_IO_PORTD         (BIT(2))
 
-#define USABLE_TIMER_CHANNEL_COUNT 12
+#define USABLE_TIMER_CHANNEL_COUNT 13
 #define USED_TIMERS ( TIM_N(1) | TIM_N(2) | TIM_N(3) | TIM_N(5) | TIM_N(8) | TIM_N(10) )
