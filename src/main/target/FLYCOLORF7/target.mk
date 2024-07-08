560cdd714ce1069314dab3c82fefaef364d0054a
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2024-07-08 13:02:16 -0500

    [target] FLYCOLORF7 add Baro bmp280 (#1072)

--- src/main/target/FLYCOLORF7/target.mk
+++ src/main/target/FLYCOLORF7/target.mk
@@ -3,6 +3,7 @@ FEATURES       += VCP ONBOARDFLASH
 
 TARGET_SRC = \
 drivers/accgyro/accgyro_spi_mpu6000.c \
+drivers/barometer/barometer_bmp280.c \
 $(addprefix drivers/compass/,$(notdir $(wildcard $(SRC_DIR)/drivers/compass/*.c))) \
 drivers/light_led.h \
 drivers/light_ws2811strip.c \
