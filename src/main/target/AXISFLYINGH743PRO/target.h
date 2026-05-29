e44208103bc6762e03836944949d90197b92d58e
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2026-05-29 17:27:44 -0500

    H7 targets

---
+++ src/main/target/AXISFLYINGH743PRO/target.h
@@ -0,0 +1,162 @@
+/*
+ * This file is part of EmuFlight. It is derived from Betaflight.
+ *
+ * This is free software. You can redistribute this software
+ * and/or modify this software under the terms of the GNU General
+ * Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later
+ * version.
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public
+ * License along with this software.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// This resource file generated using https://github.com/nerdCopter/target-convert
+// Commit: 215ae87 + 1 file changed, 31 insertions(+), 8 deletions(-)
+
+#pragma once
+
+#define BOARD_NAME         AXISFLYINGH743PRO
+#define MANUFACTURER_ID    AXFL
+#define TARGET_BOARD_IDENTIFIER "SH74"  // generic ID
+#define FC_TARGET_MCU      STM32H743     // not used in EmuF
+
+#define USE_ACC
+#define USE_ACC_SPI_ICM42688P
+#define USE_GYRO
+#define USE_GYRO_SPI_ICM42688P
+#define USE_FLASH
+#define USE_FLASH_PY25Q128HA
+#define USE_BARO
+#define USE_BARO_DPS310
+#define USE_MAX7456
+// #define USE_GYRO_CLKIN // not supported in EmuFlight
+
+#define USE_VCP
+#define USE_FLASHFS
+#define USE_FLASH_M25P16    // 16MB Micron M25P16 driver; drives all unless QSPI
+#define USE_OSD
+
+#define USE_LED
+#define LED0_PIN              PD3
+#define LED_STRIP_PIN         PE5
+#define USE_BEEPER
+#define BEEPER_PIN            PC13
+#define BEEPER_INVERTED
+
+#define USE_SPI
+#define USE_SPI_DEVICE_1
+#define SPI1_SCK_PIN          PA5
+#define SPI1_MISO_PIN        PA6
+#define SPI1_MOSI_PIN        PA7
+#define USE_SPI_DEVICE_2
+#define SPI2_SCK_PIN          PB13
+#define SPI2_MISO_PIN        PB14
+#define SPI2_MOSI_PIN        PB15
+#define USE_SPI_DEVICE_3
+#define SPI3_SCK_PIN          PB3
+#define SPI3_MISO_PIN        PB4
+#define SPI3_MOSI_PIN        PB5
+#define USE_SPI_DEVICE_4
+#define SPI4_SCK_PIN          PE12
+#define SPI4_MISO_PIN        PE13
+#define SPI4_MOSI_PIN        PE14
+
+#define USE_SPI_GYRO
+#define USE_EXTI
+#define USE_GYRO_EXTI
+
+#define ACC_1_ALIGN          CW0_DEG
+#define GYRO_1_ALIGN         CW0_DEG
+#define GYRO_1_CS_PIN         PC4
+#define GYRO_1_EXTI_PIN       PA4
+#define GYRO_1_SPI_BUS             SPIDEV_1
+
+#define USE_DUAL_GYRO
+
+#define GYRO_CONFIG_USE_GYRO_DEFAULT GYRO_CONFIG_USE_GYRO_BOTH
+#define ACC_2_ALIGN          CW90_DEG
+#define GYRO_2_ALIGN         CW90_DEG
+#define GYRO_2_CS_PIN         PE15
+#define GYRO_2_EXTI_PIN       PE11
+#define GYRO_2_SPI_BUS             SPIDEV_4
+
+#define USE_UART1
+#define UART1_TX_PIN          PB6
+#define UART1_RX_PIN          PB7
+#define USE_UART2
+#define UART2_TX_PIN          PD5
+#define UART2_RX_PIN          PD6
+#define USE_UART3
+#define UART3_TX_PIN          PD8
+#define UART3_RX_PIN          PD9
+#define USE_UART4
+#define UART4_TX_PIN          PC10
+#define UART4_RX_PIN          PC11
+#define USE_UART5
+#define UART5_RX_PIN          PD2  // ESC TELEMETRY
+#define USE_UART7
+#define UART7_TX_PIN          PE8
+#define UART7_RX_PIN          PE7
+#define USE_UART8
+#define UART8_TX_PIN          PE1
+#define UART8_RX_PIN          PE0
+#define GPS_UART                        SERIAL_PORT_USART8
+#define SERIALRX_UART                   SERIAL_PORT_USART2
+#define MSP_DISPLAYPORT_UART            SERIAL_PORT_USART1
+#define SERIAL_PORT_COUNT 8
+
+#define USE_I2C
+#define USE_I2C_DEVICE_1
+#define I2C_DEVICE_1      (I2CDEV_1)
+#define MAG_I2C_INSTANCE                I2CDEV_1
+#define I2C1_SCL PB8
+#define I2C1_SDA PB9
+#define USE_I2C_DEVICE_2
+#define I2C_DEVICE_2      (I2CDEV_2)
+#define BARO_I2C_INSTANCE               I2CDEV_2
+#define I2C2_SCL PB10
+#define I2C2_SDA PB11
+
+#define FLASH_CS_PIN          PD7
+#define FLASH_SPI_INSTANCE              SPI3
+#define ENABLE_BLACKBOX_LOGGING_ON_SPIFLASH_BY_DEFAULT
+
+#define MAX7456_SPI_CS_PIN    PB12
+#define MAX7456_SPI_INSTANCE            SPI2
+
+#define USE_ADC
+#define VBAT_ADC_PIN PC0
+#define CURRENT_METER_ADC_PIN PC1
+#define ADC1_DMA_OPT                    9
+#define ADC3_DMA_OPT                    10
+#define ADC1_DMA_STREAM DMA2_Stream1 // ADC1 opt9
+#define ADC3_DMA_STREAM DMA2_Stream2 // ADC3 opt10
+#define DEFAULT_VOLTAGE_METER_SOURCE    VOLTAGE_METER_ADC
+#define DEFAULT_CURRENT_METER_SOURCE    CURRENT_METER_ADC
+
+#define PINIO1_PIN            PE2
+#define PINIO2_PIN            PA8
+#define PINIO1_BOX                      40
+#define PINIO1_CONFIG                   129
+#define PINIO2_BOX                      41
+#define PINIO2_CONFIG                   1
+
+#define TARGET_IO_PORTA 0xffff
+#define TARGET_IO_PORTB 0xffff
+#define TARGET_IO_PORTC 0xffff
+#define TARGET_IO_PORTD 0xffff
+#define TARGET_IO_PORTE 0xffff
+// notice - port masks derived from config.h; single-pin ports use exact mask, multi-pin ports use 0xffff
+
+#define DEFAULT_FEATURES       (FEATURE_OSD | FEATURE_TELEMETRY | FEATURE_AIRMODE | FEATURE_RX_SERIAL)
+#define DEFAULT_RX_FEATURE     FEATURE_RX_SERIAL
+
+#define USABLE_TIMER_CHANNEL_COUNT 15
+#define USED_TIMERS ( TIM_N(2) | TIM_N(3) | TIM_N(4) | TIM_N(8) | TIM_N(15) )
+
+// notice - this file was programmatically generated and may be incomplete.
---
+++ src/main/target/GEPRC_TAKER_H743MINI/target.c
@@ -0,0 +1,45 @@
+/*
+ * This file is part of EmuFlight. It is derived from Betaflight.
+ *
+ * This is free software. You can redistribute this software
+ * and/or modify this software under the terms of the GNU General
+ * Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this software.
+ *
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// This resource file generated using https://github.com/nerdCopter/target-convert
+// Commit: 215ae87 + 1 file changed, 31 insertions(+), 8 deletions(-)
+
+#include <stdint.h>
+#include "platform.h"
+#include "drivers/io.h"
+#include "drivers/dma.h"
+#include "drivers/timer.h"
+#include "drivers/timer_def.h"
+
+const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
+    DEF_TIM(TIM4, CH1, PD12, TIM_USE_LED, 0, 0, NONE), // led strip
+    DEF_TIM(TIM8, CH1, PC6, TIM_USE_MOTOR, 0, 1, NONE), // motor 1
+    DEF_TIM(TIM8, CH2, PC7, TIM_USE_MOTOR, 0, 2, NONE), // motor 2
+    DEF_TIM(TIM8, CH3, PC8, TIM_USE_MOTOR, 0, 3, NONE), // motor 3
+    DEF_TIM(TIM8, CH4, PC9, TIM_USE_MOTOR, 0, 4, NONE), // motor 4
+    DEF_TIM(TIM1, CH1, PE9, TIM_USE_MOTOR, 0, 5, NONE), // motor 5
+    DEF_TIM(TIM1, CH2, PE11, TIM_USE_MOTOR, 0, 6, NONE), // motor 6
+    DEF_TIM(TIM1, CH3, PE13, TIM_USE_MOTOR, 0, 7, NONE), // motor 7
+    DEF_TIM(TIM1, CH4, PE14, TIM_USE_MOTOR, 0, 8, NONE), // motor 8
+    DEF_TIM(TIM3, CH3, PB0, TIM_USE_ANY, 0, 0, NONE), // could not determine TIM_USE_xxxxx - please check; dma -1 in config (input only)
+};
+
+// notice - this file was programmatically generated and may be incomplete.
