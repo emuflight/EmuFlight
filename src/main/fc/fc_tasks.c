595a69d2223a8d030deee1b1b5910b9a33492dff
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2026-06-02 11:28:12 -0500

    fix(fc_tasks): add missing io/flashfs.h include (#1235)
    Closes #1234.

--- src/main/fc/fc_tasks.c
+++ src/main/fc/fc_tasks.c
@@ -71,6 +71,7 @@
 #include "io/asyncfatfs/asyncfatfs.h"
 #include "io/beeper.h"
 #include "io/dashboard.h"
+#include "io/flashfs.h"
 #include "io/gps.h"
 #include "io/ledstrip.h"
 #include "io/osd.h"
