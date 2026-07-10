927547557dc52505ca4b04143bc153802baf7916
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2026-07-10 13:41:54 -0500

    targets: split OMNIBUSF4FW shared folder into atomic OMNIBUSF4FW/OMNIBUSF4V6
    Part of #1300 (atomic per-board target split).
    
    OMNIBUSF4V6 already existed as an independent target-convert-generated
    folder, shadowed by the OMNIBUSF4V6.mk marker. Its generated target.c
    mislabeled the SJ1/SJ2 backdoor pins (PC8/PC9, TIM_USE_NONE in the
    true source) as motor outputs, and was missing the CAM_CTL timer
    entry entirely (TIM11/PB7) - only 14 of the required 15 timer slots.
    Its target.h also used a single-gyro layout instead of the shared
    file's dual-gyro (onboard + external IMU) setup. Replaced both with
    reconstructions verified against the shared file's true branch.
    
    Also resolved the shared file's #if defined(OMNIBUSF4FW1) branches:
    OMNIBUSF4FW1 is referenced throughout the shared target.h/target.c but
    is never defined by any marker file or build flag - genuinely dead,
    unreachable code (per the shared file's own comment: "Not a valid
    target, .mk file must be supplied by a user"). Dropped entirely from
    both split targets.
    
    Resolved the remaining #if defined(OMNIBUSF4V6) branches (board
    identity, dual-gyro CS pin/alignment routing, I2C1 vs. I2C2 bus,
    camera control pin, current-meter scale, serial port function) into
    two independent folders.
    
    Verified BASE_TARGET resolves to OMNIBUSF4FW/OMNIBUSF4V6 respectively;
    both targets build clean.
    
    AI Generated pull-request

--- src/main/target/OMNIBUSF4V6/target.h
+++ src/main/target/OMNIBUSF4V6/target.h
@@ -1,138 +1,171 @@
 /*
- * This file is part of EmuFlight. It is derived from Betaflight.
+ * This file is part of Cleanflight and Betaflight.
  *
- * This is free software. You can redistribute this software
- * and/or modify this software under the terms of the GNU General
- * Public License as published by the Free Software Foundation,
- * either version 3 of the License, or (at your option) any later
- * version.
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * Cleanflight and Betaflight are free software. You can redistribute
+ * this software and/or modify this software under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Cleanflight and Betaflight are distributed in the hope that they
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * You should have received a copy of the GNU General Public
- * License along with this software.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software.
+ *
  * If not, see <http://www.gnu.org/licenses/>.
  */
 
-// This resource file generated using https://github.com/nerdCopter/target-convert
-// Commit: d84474d + 1 file changed, 24 deletions(-)
-
 #pragma once
 
-#define BOARD_NAME        OMNIBUSF4V6
-#define MANUFACTURER_ID   AIRB
-#define TARGET_BOARD_IDENTIFIER "S405"  // generic ID
-#define FC_TARGET_MCU     STM32F405     // not used in EmuF
+#define USE_TARGET_CONFIG
+
+#define TARGET_BOARD_IDENTIFIER "OBV6"
+
+#define USBD_PRODUCT_STRING "OmnibusF4 V6"
+
+#define LED0_PIN                PA8
+
+#define USE_BEEPER
+#define BEEPER_PIN              PB4
+#define BEEPER_INVERTED
+
+#define USE_ACC
+#define USE_ACC_SPI_MPU6000
+#define USE_ACC_SPI_MPU6500
 
 #define USE_GYRO
-#define USE_GYRO_SPI_MPU6500
 #define USE_GYRO_SPI_MPU6000
-#define USE_ACC
-#define USE_ACC_SPI_MPU6500
-#define USE_ACC_SPI_MPU6000
-#define USE_BARO_SPI_BMP280
-#define USE_MAX7456
-#define USE_BARO
-
-#define USE_VCP
-#define USE_FLASH_M25P16    // 16MB Micron M25P16 driver; drives all unless QSPI
-#define FLASH_CS_PIN         PB12
-#define FLASH_SPI_INSTANCE   SPI2
-#define ENABLE_BLACKBOX_LOGGING_ON_SPIFLASH_BY_DEFAULT
-#define USE_OSD
-
-#define USE_LED
-#define LED0_PIN             PA8
-#define LED_STRIP_PIN        PB6
-#define USE_BEEPER
-#define BEEPER_PIN           PB4
-#define BEEPER_INVERTED
-#define CAMERA_CONTROL_PIN   PB7
-
-#define USE_SPI
-#define USE_SPI_DEVICE_1
-#define SPI1_SCK_PIN         PA5
-#define SPI1_MISO_PIN        PA6
-#define SPI1_MOSI_PIN        PA7
-#define USE_SPI_DEVICE_2
-#define SPI2_SCK_PIN         PB13
-#define SPI2_MISO_PIN        PB14
-#define SPI2_MOSI_PIN        PB15
-#define USE_SPI_DEVICE_3
-#define SPI3_SCK_PIN         PC10
-#define SPI3_MISO_PIN        PC11
-#define SPI3_MOSI_PIN        PC12
-
-#define USE_SPI_GYRO
-#define USE_EXTI
-#define USE_GYRO_EXTI
-
-#define USE_MPU_DATA_READY_SIGNAL
-
-#define ACC_1_ALIGN          CW180_DEG
-#define GYRO_1_ALIGN         CW180_DEG
-#define GYRO_1_CS_PIN        PA4
-#define GYRO_1_EXTI_PIN      PC4
-#define GYRO_1_SPI_BUS SPIDEV_1
+#define USE_GYRO_SPI_MPU6500
 
 #define USE_DUAL_GYRO
 
-#define ACC_2_ALIGN          CW0_DEG
-#define GYRO_2_ALIGN         CW0_DEG
-#define GYRO_2_CS_PIN        PC14
-#define GYRO_2_SPI_BUS SPIDEV_1
+#define GYRO_1_CS_PIN           PA4   // Onboard IMU
+#define GYRO_1_SPI_BUS     SPIDEV_1
+#define GYRO_2_CS_PIN           PC14  // External IMU
+#define GYRO_2_SPI_BUS     SPIDEV_1
+
+#define GYRO_1_ALIGN            CW180_DEG
+#define ACC_1_ALIGN             CW180_DEG
+
+#define GYRO_2_ALIGN            CW0_DEG
+#define ACC_2_ALIGN             CW0_DEG
+
+#define GYRO_CONFIG_USE_GYRO_DEFAULT GYRO_CONFIG_USE_GYRO_1
+
+// MPU6000 interrupts
+#define USE_EXTI
+#define MPU_INT_EXTI            PC4
+#define USE_MPU_DATA_READY_SIGNAL
+
+#define USE_MAG
+#define MAG_I2C_INSTANCE        (I2CDEV_1)
+#define USE_MAG_HMC5883
+#define MAG_HMC5883_ALIGN       CW90_DEG
+
+#define USE_BARO
+#define BARO_I2C_INSTANCE       (I2CDEV_1)
+#define USE_BARO_SPI_BMP280
+#define BMP280_SPI_INSTANCE     SPI3
+#define BMP280_CS_PIN           PB3
+#define DEFAULT_BARO_SPI_BMP280
+
+#define USE_MAX7456
+#define MAX7456_SPI_INSTANCE    SPI3
+#define MAX7456_SPI_CS_PIN      PA15
+#define MAX7456_SPI_CLK         (SPI_CLOCK_STANDARD) // 10MHz
+#define MAX7456_RESTORE_CLK     (SPI_CLOCK_FAST)
+
+#define USE_FLASHFS
+#define USE_FLASH_M25P16
+#define USE_FLASH_W25Q128FV
+#define ENABLE_BLACKBOX_LOGGING_ON_SPIFLASH_BY_DEFAULT
+#define FLASH_SPI_INSTANCE      SPI2
+#define FLASH_CS_PIN            PB12
+
+#define USE_VCP
+#define VBUS_SENSING_PIN        PC5
 
 #define USE_UART1
-#define UART1_TX_PIN         PA9
-#define UART1_RX_PIN         PA10
-#define USE_UART2
-#define UART2_TX_PIN         PA2
-#define USE_UART3
-#define UART3_TX_PIN         PB10
-#define UART3_RX_PIN         PB11
-#define USE_UART4
-#define UART4_RX_PIN         PA1
-#define USE_UART6
-#define UART6_TX_PIN         PC6
-#define UART6_RX_PIN         PC7
-#define SERIAL_PORT_COUNT 6
+#define UART1_RX_PIN            PA10  // Inverted serial RX
+#define UART1_TX_PIN            PA9   // Non-inverted Half-duplex (exclusive with serial RX
 
-#define BARO_CS_PIN          PB3
-#define BARO_SPI_INSTANCE SPI3
-#define BMP280_CS_PIN       PB3
-#define BMP280_SPI_INSTANCE SPI3
+#define USE_UART2
+#define UART2_RX_PIN            NONE
+#define UART2_TX_PIN            PA2   // Half-duplex
+
+#define USE_UART3
+#define UART3_RX_PIN            PB11  // Shared with I2C2 (SDA)
+#define UART3_TX_PIN            PB10  // Shared with I2C2 (SCL)
+
+#define USE_UART4
+#define UART4_RX_PIN            PA1   // Simplex RX, ESC telemetry
+#define UART4_TX_PIN            NONE
+
+#define USE_UART6
+#define UART6_RX_PIN            PC7
+#define UART6_TX_PIN            PC6
+
+#define USE_SOFTSERIAL1
+//#define SOFTSERIAL1_TX_PIN      PA9   // SmartPort, shared with UART1_TX, doesn't work (timer collision)
+
+#define USE_SOFTSERIAL2
+
+#define SERIAL_PORT_COUNT       8 // VCP, UART1,2,3,4,6, SOFTSERIAL x 2
+
+#define USE_ESCSERIAL
+#define ESCSERIAL_TIMER_TX_PIN  PB8  // (PPM)
+
+#define USE_SPI
+#define USE_SPI_DEVICE_1
+#define SPI1_SCK_PIN            PA5
+#define SPI1_MISO_PIN           PA6
+#define SPI1_MOSI_PIN           PA7
+
+#define USE_SPI_DEVICE_2
+#define SPI2_NSS_PIN            PB12
+#define SPI2_SCK_PIN            PB13
+#define SPI2_MISO_PIN           PB14
+#define SPI2_MOSI_PIN           PB15
+
+#define USE_SPI_DEVICE_3
+#define SPI3_NSS_PIN            PA15
+#define SPI3_SCK_PIN            PC10
+#define SPI3_MISO_PIN           PC11
+#define SPI3_MOSI_PIN           PC12
+
 #define USE_I2C
 #define USE_I2C_DEVICE_1
-#define I2C_DEVICE_1      (I2CDEV_1)
-#define MAG_I2C_INSTANCE (I2CDEV_1)
-#define DASHBOARD_I2C_INSTANCE (I2CDEV_1)
-#define I2C1_SCL PB8
-#define I2C1_SDA PB9
+#define I2C1_SCL                PB8 // SCL PIN,alt MST8
+#define I2C1_SDA                PB9 // SDA PIN,alt MST7
+#define I2C_DEVICE              (I2CDEV_1)
 
-#define MAX7456_SPI_CS_PIN   PA15
-#define MAX7456_SPI_INSTANCE SPI3
+#define CAMERA_CONTROL_PIN      PB7
 
 #define USE_ADC
-#define VBAT_ADC_PIN PC2
-#define CURRENT_METER_ADC_PIN PC1
-#define RSSI_ADC_PIN PA0
-#define ADC2_DMA_OPT        1
-#define ADC2_DMA_STREAM DMA2_Stream3 //# ADC 2: DMA2 Stream 3 Channel 1
+#define ADC_INSTANCE            ADC2
+#define CURRENT_METER_ADC_PIN   PC1  // Direct from CRNT pad (part of onboard sensor for Pro)
+#define VBAT_ADC_PIN            PC2  // 11:1 (10K + 1K) divider
+#define RSSI_ADC_PIN            PA0  // Direct from RSSI pad
+
 #define DEFAULT_VOLTAGE_METER_SOURCE VOLTAGE_METER_ADC
 #define DEFAULT_CURRENT_METER_SOURCE CURRENT_METER_ADC
-#define ADC_INSTANCE ADC2
 
+#define USE_TRANSPONDER
 
-#define TARGET_IO_PORTA 0xffff
-#define TARGET_IO_PORTB 0xffff
-#define TARGET_IO_PORTC 0xffff
-// notice - masks were programmatically generated - please verify last port group for 0xffff or (BIT(2))
+#define USE_SONAR
 
-#define DEFAULT_FEATURES       (FEATURE_OSD | FEATURE_TELEMETRY | FEATURE_AIRMODE | FEATURE_RX_SERIAL)
-#define DEFAULT_RX_FEATURE     FEATURE_RX_SERIAL
+#define DEFAULT_RX_FEATURE      FEATURE_RX_SERIAL
 
-#define USABLE_TIMER_CHANNEL_COUNT 14
-#define USED_TIMERS ( TIM_N(1) | TIM_N(2) | TIM_N(3) | TIM_N(4) | TIM_N(5) | TIM_N(8) | TIM_N(9) | TIM_N(10) )
+#define DEFAULT_FEATURES        (FEATURE_OSD)
 
-// notice - this file was programmatically generated and may be incomplete.
+#define TARGET_IO_PORTA (0xffff & ~(BIT(14)|BIT(13)))
+#define TARGET_IO_PORTB (0xffff & ~(BIT(2)))
+#define TARGET_IO_PORTC  0xffff
+#define TARGET_IO_PORTD BIT(2)
+
+#define USABLE_TIMER_CHANNEL_COUNT 15
+
+#define USED_TIMERS ( TIM_N(1) | TIM_N(2) | TIM_N(3) | TIM_N(4) | TIM_N(5) | TIM_N(8) | TIM_N(9) | TIM_N(10) | TIM_N(11))
